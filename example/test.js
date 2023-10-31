4e8ef6dbc56f8b0b0e9528917badbd1fb900f986
whosejam <276707964@qq.com>
2023-10-31 18:57:32 +0800

    path move

--- example/test.js
+++ example/test.js
@@ -1,27 +1,14 @@
 import * as sd from "#lib/slide";
 
 let svg = sd.svg();
-let poly = sd.Polyline(svg);
-poly.points([100, 100, 200, 100, 50, 50, 100, 100]).drag(true);
+let path = sd.Path(svg);
+path.d("M 50 50 H 150 V 150").drag(true);
 
 main();
 
 async function main() {
-    // await sd.pause();
-    // poly.startAnimate().x(300).endAnimate();
     await sd.pause();
-    poly.startAnimate()
-        .points([100, 100, 200, 100, 150, 50, 100, 100])
-        .endAnimate();
-    await sd.pause();
-    poly.startAnimate()
-        .width(200)
-        .height(200)
-        .endAnimate();
-    // poly.startAnimate()
-    //     .y(300)
-    //     .endAnimate();
-    // poly.startAnimate()
-    //     .x(300)
-    //     .endAnimate();
+    path.startAnimate();
+    path.x(300);
+    path.endAnimate();
 }
