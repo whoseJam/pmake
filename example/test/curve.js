189b55fbc2bbad3429f2fe7fe7263ba38fa0c5ab
whosejam <276707964@qq.com>
2023-11-01 10:43:15 +0800

    bezier

--- example/test/curve.js
+++ example/test/curve.js
@@ -3,7 +3,7 @@ import * as sd from "#lib/slide";
 let svg = sd.svg();
 // let c = sd.Curve(svg).drag(true);
 // c.x1(100).y1(100).x2(300).y2(200);
-let c2 = sd.VHCurve(svg).drag(true).resizeable(true);
+let c2 = sd.Bezier(svg).drag(true).resizeable(true);
 c2.x1(500).y1(200).x2(800).y2(400).strokeWidth(5);
 
 async function main() {
