1ae56a9b29a4ce06f92347637d79dbdc1e50e328
whosejam <276707964@qq.com>
2023-11-08 19:21:53 +0800

    dijkstra

--- example/test.js
+++ example/test.js
@@ -1,35 +1,12 @@
-import dagre from "dagre";
+import * as sd from "#lib/slide";
 
-var g = new dagre.graphlib.Graph();
+let svg = sd.svg();
+sd.Circle(svg).cx(100).cy(100);
+let math = sd.Mathjax(svg).height(100).math("\\infty").drag(true);//.height(100)
 
-// Set an object for the graph label
-g.setGraph({});
+main();
 
-// Default to assigning a new object as a label for each new edge.
-g.setDefaultEdgeLabel(function() { return {}; });
-
-// Add nodes to the graph. The first argument is the node id. The second is
-// metadata about the node. In this case we're going to add labels to each of
-// our nodes.
-g.setNode("kspacey",    { label: "Kevin Spacey",  width: 144, height: 100 });
-g.setNode("swilliams",  { label: "Saul Williams", width: 160, height: 100 });
-g.setNode("bpitt",      { label: "Brad Pitt",     width: 108, height: 100 });
-g.setNode("hford",      { label: "Harrison Ford", width: 168, height: 100 });
-g.setNode("lwilson",    { label: "Luke Wilson",   width: 144, height: 100 });
-g.setNode("kbacon",     { label: "Kevin Bacon",   width: 121, height: 100 });
-
-// Add edges to the graph.
-g.setEdge("kspacey",   "swilliams");
-g.setEdge("swilliams", "kbacon");
-g.setEdge("bpitt",     "kbacon");
-g.setEdge("hford",     "lwilson");
-g.setEdge("lwilson",   "kbacon");
-
-dagre.layout(g);
-
-g.nodes().forEach(function(v) {
-    console.log("Node " + v + ": " + JSON.stringify(g.node(v)));
-});
-g.edges().forEach(function(e) {
-   console.log("Edge " + e.v + " -> " + e.w + ": " + JSON.stringify(g.edge(e)));
-});
+async function main() {
+    await sd.pause();
+    math.startAnimate().math("1").endAnimate();
+}
--- example/test/test.js
+++ example/test/test.js
@@ -9,7 +9,7 @@ let LY, RY, Y;
 main();
 
 async function main() {
-    await testArray();
+    // await testArray();
     // await testMoreArray();
     // await testMathjax();
     // await testCode();
@@ -17,12 +17,134 @@ async function main() {
     // await testGrid();
     // await testVarTable();
     // await testCallStack();
-    // await testCurve();
-    // await testGraph();
+    await testCurve();
+    await testGraph();
 }
 
 async function testGraph() {
+    await sd.pause();
+    let title = L.Title("Graph组件展示");
 
+    let rule = sd.rule();
+    let H = sd.helper();
+    let C = sd.color();
+    let g = sd.Graph(svg);
+    let n = 7;
+    let m = 11;
+    let edges = H.ForwardStar();
+    let e = [
+        [1, 2, 12],
+        [1, 6, 16],
+        [1, 7, 14],
+        [2, 3, 10],
+        [2, 6, 7],
+        [3, 4, 3],
+        [6, 3, 6],
+        [5, 4, 4],
+        [6, 5, 2],
+        [7, 5, 8],
+        [7, 6, 9]
+    ];
+    for (let i = 1; i <= n; i++) g.newNode(i, i);
+    for (let i = 0; i < m; i++) {
+        g.newLink(e[i][0], e[i][1], sd.Text(svg, e[i][2]).fontSize(25));
+        edges.link(e[i][0], e[i][1], e[i][2]);
+        edges.link(e[i][1], e[i][0], e[i][2]);
+    }
+    g.width(500).height(500).cx(300).cy(300).drag(true);
+    await solve();
+    async function solve() {
+        await sd.pause();
+        for (let i = 1; i <= n; i++) {
+            let table = sd.VarTable(svg);
+            table.put("dis", Infinity);
+            table.put("vis", "false");
+            table.drag(true);
+            g.element(i).children.push(
+                "table", table,
+                rule.onRightSide(),
+            );
+            table.opacity(0).startAnimate().opacity(1).endAnimate();
+        }
+        await Dijkstra(1);
+    }
+    
+    function setDis(at, newDis) {
+        g.startAnimate()
+        g.element(at).children.child("table").put("dis", newDis)
+        g.endAnimate();
+    }
+    
+    function getDis(at) {
+        return +g.element(at).children.child("table").find("dis").text();
+    }
+    
+    function getEdge(x, y) {
+        let l;
+        try {
+            l = g.element(x, y);
+        } catch(e) {
+            l = g.element(y, x);
+        }
+        return l;
+    }
+    
+    async function Dijkstra(S) {
+        await sd.pause();
+        g.startAnimate().color(S, C.green).endAnimate();
+        await sd.pause();
+        setDis(S, 0);
+        await sd.pause();
+        g.startAnimate().color(S, C.white).endAnimate();
+        let vis = sd.make1d(100, 0);
+        for (let i = 1; i <= n; i++) {
+            let cur = 0, curDis = Infinity;
+            for (let j = 1; j <= n; j++) {
+                if (vis[j]) continue;
+                if (curDis > getDis(j)) {
+                    curDis = getDis(j);
+                    cur = j;
+                }
+            }
+            await sd.pause();
+            g.startAnimate();
+            g.color(cur, C.orange)
+            g.element(cur).children.child("table").put("vis", "true");
+            g.endAnimate();
+            vis[cur] = true;
+            
+            let adj = edges.adjacent(cur);
+            for (let j = 0; j < adj.length; j++) {
+                let v = adj[j].to, w = adj[j].value;
+                if (vis[v]) continue;
+                await sd.pause();
+                g.startAnimate().color(v, C.blue).endAnimate();
+                await sd.pause();
+                g.startAnimate();
+                let l = getEdge(cur, v);
+                l.strokeWidth(3)
+                l.stroke(C.red);
+                g.endAnimate();
+                await sd.pause();
+                let newDis = Math.min(getDis(cur) + w, getDis(v));
+                setDis(v, newDis);
+                await sd.pause();
+                g.startAnimate()
+                g.color(v, C.white);
+                l.strokeWidth(1).stroke(C.black);
+                g.endAnimate();
+            }
+            await sd.pause();
+            g.startAnimate()
+            g.color(cur, C.grey);
+            g.element(cur).children.child("table").color(C.grey);
+            g.endAnimate();
+        }
+    }
+    await sd.pause();
+    let deletes = [title, g];
+    for (let i = 0; i < deletes.length; i++)
+        deletes[i].startAnimate().opacity(0).endAnimate().remove();
 }
 
 async function testCurve() {
@@ -94,6 +216,7 @@ async function testCurve() {
         cc.cx(movePoint[0])
         cc.cy(movePoint[1]);
     }, 1000, mina.easeinout);
+    await sd.pause();
 
     let deletes = [c1, c2, c3, c4, cc, para1, title];
     for (let i = 0; i < deletes.length; i++)
@@ -393,8 +516,8 @@ async function testTree() {
                 }
             }
             if (sn[now]) {
-                tree1.element(now, sn[now]).background().strokeWidth(5);
-                tree1.element(now, sn[now]).background().stroke(C.red);
+                tree1.element(now, sn[now]).strokeWidth(5);
+                tree1.element(now, sn[now]).stroke(C.red);
             }
         }
         async function dfs2(now, prt) {
