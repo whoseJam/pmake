cb3342d14a492ef7e56a6d1ade3a09a84b0d77ea
whosejam <276707964@qq.com>
2024-02-13 17:00:03 +0800

    rollback & forward

--- example/test.js
+++ example/test.js
@@ -1,14 +1,13 @@
 import * as sd from "../lib/slide";
 
 let svg = sd.svg();
-let lk = new sd.Rect(svg);//.push(1).push(2).push(3);
+let lk = new sd.Array(svg).push(1).push(2).push(3);
+let r = new sd.Rect(svg).width(5).height(5).cx(100).cy(100);
 
 main();
 
 async function main() {
     await sd.pause();
-    console.log("---------------------------");
-    lk.startAnimate(1000).y(100).endAnimate();
-    lk.after(500).startAnimate(1000).y(0).endAnimate();
+    lk.startAnimate().cx(100).cy(100).endAnimate();
     await sd.pause();
 }
