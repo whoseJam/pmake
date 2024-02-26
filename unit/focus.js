1033c887691d02032c03c6a7ce84fab8ba55a9f9
whosejam <276707964@qq.com>
2024-02-26 11:13:49 +0800

    add focus

---
+++ unit/focus.js
@@ -0,0 +1,14 @@
+import * as sd from "../lib/slide";
+
+let svg = sd.svg();
+let arr = new sd.Array(svg).x(100).y(100).resize(10);
+let f = sd.Focus(arr);
+
+main();
+
+async function main() {
+    for (let i = arr.start(); i <= arr.end(); i++) {
+        await sd.pause();
+        f.startAnimate().focus(i).endAnimate();
+    }
+}
