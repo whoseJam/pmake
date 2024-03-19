fa1b0c5ab0c4f5b41d9c3fb86c0f9c9b12af2974
whosejam <276707964@qq.com>
2024-03-19 11:44:14 +0800

    remove dirty

--- unit/box.js
+++ unit/box.js
@@ -2,8 +2,9 @@ import * as sd from "../lib/slide";
 
 let svg = sd.svg();
 let C = sd.color();
+let R = sd.rule();
 let board = new sd.Text(svg);
-let obj = new sd.Box(svg).value(new sd.Circle(svg).color(C.BLUE)).cx(600).cy(300);
+let obj = new sd.Box(svg).value(new sd.Circle(svg).color(C.BLUE), R.CenterOnly()).cx(600).cy(300);
 let value;
 
 main();
@@ -17,7 +18,7 @@ async function main() {
     await sd.pause();
     test(`obj.startAnimate().x(100).y(100).endAnimate();`);
     await sd.pause();
-    test(`obj.startAnimate().width(80).endAnimate();`);
+    test(`obj.startAnimate().width(100).height(200).endAnimate();`);
     await sd.pause();
     test(`obj.startAnimate().value(null).endAnimate();`);
     await sd.pause();
