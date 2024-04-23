1858e55edef28b95a173f977dbaca6f2eafbad2e
whosejam <276707964@qq.com>
2024-04-23 14:22:29 +0800

    [feat]新的array

--- unit/array.js
+++ unit/array.js
@@ -6,7 +6,7 @@ let arr = new sd.Array(svg).x(100).y(100);
 main();
 
 async function main() {
-    arr.push(1).push(2).push(3);
+    arr.push(1).push(2).push(3).update();
     await sd.pause();
     arr.startAnimate().insert(4, "+1").insert(1, "+2").endAnimate();
     await sd.pause();
