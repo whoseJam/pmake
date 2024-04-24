ad2eeb5d48c9b7027e5dcbc375f7aa0193a2cf6c
whosejam <276707964@qq.com>
2024-04-24 22:36:21 +0800

    [feat]Animate

--- unit/action.js
+++ unit/action.js
@@ -1,17 +1,26 @@
 import * as sd from "../lib/slide";
 
 let svg = sd.svg();
-let r = new sd.Rect(svg).x(100).y(100);
+let r = new sd.Mathjax(svg, "A^2+B^2=C^2").width(100).cx(600).cy(300);
 
 main();
 
 async function main() {
-    global.move = async function() {
-        await sd.pause();
-        r.startAnimate().dx(50).endAnimate();
-        await sd.pause();
-        r.startAnimate().dx(50).endAnimate();
-        await sd.pause();
-        r.startAnimate().dx(50).endAnimate();
-    }
+    await sd.pause();
+    console.log("--------------start----------------");
+    r.startAnimate(600)
+    r.endAnimate()
+    r.after(0)
+
+    r.startAnimate()
+    r.width(200).cx(600);
+    r.endAnimate()
+
+    r.startAnimate()
+    r.width(100).cx(600);
+    r.endAnimate()
+
+    r.after(0)
+    r.startAnimate(600);
+    await sd.pause();
 }
