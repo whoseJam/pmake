9e406ee93dfeb8d1a9710af857619bd3e9f7b3d8
whosejam <276707964@qq.com>
2024-05-06 21:30:30 +0800

    [style]添加ts-check

--- unit/test.js
+++ unit/test.js
@@ -1,19 +1,37 @@
+// @ts-check
 import * as sd from "../lib/slide";
 
-const p = Promise.resolve();
-export function nextTick(fn) {
-	return fn ? p.then(fn) : p;
+/**
+ * @description 对两个数字进行相加
+ * @overload
+ * @param {number} x - 第一个数字
+ * @param {number} y - 第二个数字
+ * @returns {number} 两个数字相加的结果
+ * 
+ * @description 将两个字符串进行拼接
+ * @overload
+ * @param {string} x - 第一个字符串
+ * @param {string} y - 第二个字符串
+ * @returns {string} 两个字符串拼接的结果
+ */
+function addOrConcatenate(x, y) {
+    if (typeof x === 'number' && typeof y === 'number') {
+      return x + y;
+    } else if (typeof x === 'string' && typeof y === 'string') {
+      return x + y;
+    }
 }
+
+addOrConcatenate(1, 2)
+
+let svg = sd.svg();
+const t = new sd.Tree(svg);
+
+const e = t.element(1, 2);
+t.value(1, "hello");
 
 main();
 
 async function main() {
-    await sd.pause();
-    nextTick(function() {
-        console.log("in nextTick");
-    })
-    for (let i = 1; i <= 10; i++)
-        console.log(i)
-    await sd.pause();
-    r.startAnimate(1000).color(C.red).endAnimate();
+
 }
--- work/拓扑排序/test.js
+++ work/拓扑排序/test.js
@@ -3,6 +3,5 @@ import * as sd from "../../lib/slide";
 
 const svg = sd.svg();
 const C = sd.color();
-const r = new sd.Rect(svg);
-r.fill(C.black);
-r.fill(C.BLUE).fillOpacity();
+const r = new sd.Circle(svg);
+r.x(2);
