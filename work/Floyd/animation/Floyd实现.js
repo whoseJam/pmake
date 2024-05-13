821e17d06a946f2179bdd533872720f06c84f1fe
whosejam <276707964@qq.com>
2024-05-13 19:35:08 +0800

    [work]Floyd

---
+++ work/Floyd/animation/Floyd实现.js
@@ -0,0 +1,31 @@
+import * as sd from "../../../lib/slide";
+import { floyd } from "./Floyd";
+
+const svg = sd.svg();
+const graph = new sd.GridGraph(svg).n(1).m(1).width(200).height(200).cx(400).cy(300);
+
+init();
+main();
+
+function init() {
+    graph.at(0, 0.5).newNode(1);
+    graph.at(0.5, 0).newNode(2);
+    graph.at(0.5, 1).newNode(3);
+    graph.at(1, 0.5).newNode(4);
+    const data = [
+        [1, 2, 3],
+        [1, 3, 8],
+        [2, 3, 1],
+        [2, 4, 6],
+        [3, 4, 2]
+    ];
+    data.forEach(item => {
+        graph.newLink(item[0], item[1], item[2]);
+        graph.element(item[0], item[1]).arrow();
+    });
+}
+
+async function main() {
+    await floyd(graph);
+    await sd.pause();
+}
