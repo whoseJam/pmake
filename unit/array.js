c092c76b9b69836f0dac208b765b6fc1dab18b11
whosejam <276707964@qq.com>
2024-05-16 14:24:00 +0800

    [feat]更新所有Array元素的dirty系统

--- unit/array.js
+++ unit/array.js
@@ -1,23 +1,44 @@
 import * as sd from "../lib/slide";
 
-let svg = sd.svg();
-let arr = new sd.Array(svg).x(100).y(100);
-// let Arr = new sd.Array(svg).x(100).y(200);
+const svg = sd.svg();
 
 main();
 
 async function main() {
+    const arr = new sd.Array(svg).x(800).y(100);
     arr.push(1).push(2).push(3).push(4);
     await sd.pause();
     arr.startAnimate()
     arr.insert(1, "+2");
     arr.insert(4, "+1");
     arr.endAnimate();
-    // arr.startAnimate();
-    // arr.insert(3, "inf");
-    // arr.endAnimate();
+    arr.startAnimate();
+    arr.insert(3, "inf");
+    arr.endAnimate();
     await sd.pause();
-    arr.startAnimate(500).erase(2).endAnimate();
-    arr.startAnimate(500).erase(3).endAnimate();
+    arr.startAnimate().erase(2).endAnimate();
+    arr.startAnimate().erase(3).endAnimate();
     await sd.pause();
+
+    const a1 = new sd.Array(svg).x(100).y(100).push(1).push(2).push(3);
+    const a2 = new sd.Array(svg).x(100).y(150).push(4).push(5).push(6);
+    await sd.pause();
+    const e1 = a1.dropElement(2);
+    a2.startAnimate().pushFromExistElement(e1).endAnimate();
+    await sd.pause();
+
+    const a3 = new sd.Array(svg).x(100).y(220).push(1).push(2).push(3);
+    const a4 = new sd.Array(svg).x(100).y(270).push(4).push(5).push(6);
+    await sd.pause();
+    const e3 = a3.dropValue(2);
+    a4.startAnimate(1000).pushFromExistValue(e3).endAnimate();
+    await sd.pause();
+
+    const a5 = new sd.Array(svg).x(400).y(100).push(1).push(2).push(3);
+    const a6 = new sd.Array(svg).x(400).y(150).push(4).push(5).push(6);
+    await sd.pause();
+    const e5 = a5.startAnimate().dropValue(2).endAnimate();
+    a6.startAnimate().pushFromExistValue(e5).endAnimate();
+    await sd.pause();
+
 }
