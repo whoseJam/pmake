705cfe7db9a4e5176d1dd4ce717e80b02f3a98f8
whosejam <276707964@qq.com>
2024-06-05 23:39:36 +0800

    [work]lca

---
+++ work/LCA/animation/LCA概念.js
@@ -0,0 +1,65 @@
+import * as sd from "@/slide";
+
+const svg = sd.svg();
+const C = sd.color();
+const tree = new sd.Tree(svg);
+const n = 7;
+const links = [
+    [1, 2], [1, 3],
+    [2, 4], [2, 5],
+    [3, 6],
+    [5, 7]
+];
+
+init();
+main();
+
+function init() {
+    tree.root(1);
+    links.forEach(link => {
+        tree.link(link[0], link[1]);
+    });
+    tree.cx(600).cy(300);
+}
+
+async function main() {
+    await showAncestorAndLCA(4, 7);
+    await showAncestorAndLCA(2, 6);
+    await showAncestorAndLCA(2, 7);
+    await sd.pause();
+}
+
+async function showAncestorAndLCA(x, y) {
+    for (let i = 1; i <= n; i++)
+        tree.element(i).mark = 0;
+    await sd.pause();
+    const px = sd.Pointer(tree, "x", "r").startAnimate().moveTo(x).endAnimate();
+    const py = sd.Pointer(tree, "y", "l").startAnimate().moveTo(y).endAnimate();
+    await sd.pause();
+    climb(x); climb(y);
+    let lca = undefined;
+    for (let i = 1; i <= n; i++) {
+        const e = tree.element(i);
+        if (e.mark === 2) {
+            e.startAnimate().color(C.blue).endAnimate();
+            if (!lca || tree.depth(lca) < tree.depth(i))
+                lca = i;
+        }
+    }
+    await sd.pause();
+    tree.startAnimate().color(lca, C.green).endAnimate();
+    await sd.pause();
+    px.startAnimate().opacity(0).remove();
+    py.startAnimate().opacity(0).remove();
+    tree.startAnimate().color(C.white).endAnimate();
+}
+
+function climb(x) {
+    let cnt = 0;
+    while (x && cnt <= 10) {
+        tree.element(x).mark++;
+        x = tree.father(x);
+        if (!x) break; x = x.nodeId;
+        cnt++;
+    }
+}
---
+++ work/LCA/animation/LCA预处理.js
@@ -0,0 +1,82 @@
+import * as sd from "@/slide";
+
+const svg = sd.svg();
+const C = sd.color();
+const tree = new sd.Tree(svg).width(600).layerHeight(50);
+const n = 10;
+const m = 3;
+const st = new sd.Grid(svg).n(m).m(n).startM(1);
+const fa = sd.make2d(20, 10, 0);
+const links = [
+    [1, 2], [1, 3],
+    [2, 4], [2, 5],
+    [3, 6], [5, 7],
+    [7, 8], [7, 9], [9, 10]
+];
+
+init();
+main();
+
+function init() {
+    tree.root(1);
+    links.forEach(link => {
+        tree.link(link[0], link[1]);
+    });
+    tree.cx(600).cy(200);
+    for (let i = 1; i <= n; i++) {
+        st.children.push(new sd.Text(st, i).fontSize(20), function(parent, child) {
+            const elem = st.element(m - 1, i);
+            child.cx(elem.cx());
+            child.y(elem.my() + 3);
+        })
+    }
+    for (let i = 0; i < m; i++) {
+        st.children.push(new sd.Mathjax(st).math(`2^${i}`).height(20), function(parent, child) {
+            const elem = st.element(m - 1 - i, 1);
+            child.mx(elem.x() - 5);
+            child.cy(elem.cy());
+        })
+    }
+    st.cx(600).y(tree.my() + 40);
+}
+
+async function main() {
+    for (let i = 2; i <= n; i++)
+        await prepare(i);
+    await sd.pause();
+}
+
+async function prepare(u) {
+    await sd.pause();
+    fa[u][0] = tree.father(u)?.nodeId;
+    if (!fa[u][0]) fa[u][0] = 0;
+    const nodeU = tree.element(u);
+    const ls = [];
+    for (let i = 0; i < m; i++) {
+        if (i > 0) fa[u][i] = fa[fa[u][i-1]][i-1];
+        if (fa[u][i]) {
+            const nodeFa = tree.element(fa[u][i]);
+            const l = sd.Link(nodeU, nodeFa, sd.Curve).bending(-0.5);
+            nodeU.update();
+            l.startAnimate()
+                .pointStoT()
+                .endAnimate()
+                .arrow();
+            ls.push(l);
+        }
+    }
+    await sd.pause();
+    st.startAnimate();
+    for (let i = 0; i < m; i++) {
+        if (fa[u][i]) {
+            st.color(m - i - 1, u, C.green);
+            st.value(m - i - 1, u, fa[u][i]);
+        }
+    }
+    st.endAnimate();
+    await sd.pause();
+    st.startAnimate().color(C.white).endAnimate();
+    ls.forEach(l => {
+        l.startAnimate().opacity(0).remove();
+    });
+}
