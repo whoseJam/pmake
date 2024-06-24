6cf0c6a5ab638e93fe4d20e2f13c5c49a62b54de
whosejam <276707964@qq.com>
2024-06-25 00:33:00 +0800

    [work]BSGS

--- asset/slide/initComponent.js
+++ asset/slide/initComponent.js
@@ -5,46 +5,63 @@ function replace(parent, oldElement, newElement) {
 
 function initCodeblock() {
     const elements = document.getElementsByTagName("codeblock");
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        const parent = element.parentNode;
-        const script = element.getElementsByTagName("script")[0];
-        let lang = element.getAttribute("lang");
-        if (!lang) lang = "cpp";
-        const pre = document.createElement("pre");
-        const code = document.createElement("code");
-        code.setAttribute("data-trim", "");
-        code.setAttribute("data-line-numbers", "");
-        code.setAttribute("class", lang);
-        pre.append(code);
-        code.append(script);
-        replace(parent, element, pre);
-    }
+    if (elements.length === 0) return;
+    const element = elements[0];
+    const parent = element.parentNode;
+    const script = element.getElementsByTagName("script")[0];
+    let lang = element.getAttribute("lang");
+    if (!lang) lang = "cpp";
+    const pre = document.createElement("pre");
+    const code = document.createElement("code");
+    pre.className = element.className;
+    code.setAttribute("data-trim", "");
+    code.setAttribute("data-line-numbers", "");
+    code.setAttribute("class", lang);
+    pre.append(code);
+    code.append(script);
+    replace(parent, element, pre);
+    initCodeblock();
 }
 
 function initPicture() {
     const elements = document.getElementsByTagName("picture");
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        console.log("element = ", element);
-        const parent = element.parentNode;
-        let src = element.getAttribute("src");
-        if (!src) src = element.getAttribute("data-src");
-        const width = element.getAttribute("width");
-        const height = element.getAttribute("height");
+    if (elements.length === 0) return;
+    const element = elements[0];
+    const parent = element.parentNode;
+    let src = element.getAttribute("src");
+    if (!src) src = element.getAttribute("data-src");
+    const width = element.getAttribute("width");
+    const height = element.getAttribute("height");
 
-        const div = document.createElement("div");
-        const image = document.createElement("img");
-        image.setAttribute("data-src", src);
-        if (width) image.setAttribute("width", width);
-        if (height) image.setAttribute("height", height);
-        div.style["textAlign"] = "center";
-        div.append(image);
-        replace(parent, element, div);
-    }
+    const div = document.createElement("div");
+    const image = document.createElement("img");
+    image.setAttribute("data-src", src);
+    if (width) image.setAttribute("width", width);
+    if (height) image.setAttribute("height", height);
+    div.className = element.className;
+    div.style["textAlign"] = "center";
+    div.append(image);
+    replace(parent, element, div);
+    initPicture();
+}
+
+function initProblem() {
+    const elements = document.getElementsByTagName("problem");
+    if (elements.length === 0) return;
+    const element = elements[0];
+    const parent = element.parentNode;
+    let src = element.getAttribute("src");
+    if (!src) src = element.getAttribute("data-src");
+
+    const section = document.createElement("section");
+    section.setAttribute("data-background-iframe", src);
+    section.setAttribute("data-background-interactive", "");
+    replace(parent, element, section);
+    initProblem();
 }
 
 export function initComponent() {
     initPicture();
     initCodeblock();
+    initProblem();
 }
