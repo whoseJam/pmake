749cf77c81d904884c1486cc0fb74088b82bcaa3
whosejam <276707964@qq.com>
2024-07-02 00:23:26 +0800

    [work]筛法

--- work/线性筛/std/筛法其四.cpp
+++ work/线性筛/std/筛法其四.cpp
@@ -35,8 +35,7 @@ void Sieve(int n){
 		for(int j=1;j<=tot&&i*prim[j]<=n;j++){
 			vis[i*prim[j]]=1;
 			if(i%prim[j]==0){
-				int tmp=f[i]/(cnt[i]+1);
-				f[i*prim[j]]=tmp*(cnt[i]+2);
+				f[i*prim[j]]=f[i]/(cnt[i]+1)*(cnt[i]+2);
 				cnt[i*prim[j]]=cnt[i]+1;
 				break;
 			}
