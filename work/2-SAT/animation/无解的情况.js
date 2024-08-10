e3dd7d7fe6890f2c7ec76c29dc4569d26091a458
whosejam <276707964@qq.com>
2024-08-10 18:55:46 +0800

    [work]2-SAT

---
+++ work/2-SAT/animation/无解的情况.js
@@ -0,0 +1,28 @@
+import * as sd from "@/sd";
+
+const svg = sd.svg();
+const graph = new sd.GridGraph(svg).width(150).height(90);
+
+init();
+main();
+
+function init() {
+    graph.at(0.5, 0).newNode(1, new sd.Mathjax(graph, "Y_i"));
+    graph.at(0.5, 1).newNode(2, new sd.Mathjax(graph, "N_i"));
+    graph.element(1).rate(2);
+    graph.element(2).rate(2);
+    graph.at(0, 0.5).newNode(3, "...");
+    graph.at(1, 0.5).newNode(4, "...");
+    graph.element(3).strokeOpacity(0);
+    graph.element(4).strokeOpacity(0);
+    function link(a, b) {
+        graph.link(a, b);
+        graph.element(a, b).arrow();
+    }
+    link(1, 3); link(3, 2);
+    link(2, 4); link(4, 1);
+}
+
+async function main() {
+    await sd.pause();
+}
