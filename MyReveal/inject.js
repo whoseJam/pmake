b7b948ac142acfc42973fa32536af5f0eae8a78e
whosejam <276707964@qq.com>
2024-08-12 22:14:17 +0800

    [feat]允许部分html加载失败

--- MyReveal/inject.js
+++ MyReveal/inject.js
@@ -12,21 +12,28 @@ export function includeHTML(callback) {
         }
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-                const parser = new DOMParser();
-                const htmlString = this.responseText;
-                const doc = parser.parseFromString(htmlString, "text/html");
-                const body = doc.documentElement.children[1];
-                const parent = element.parentNode;
-                if (!parent) {
-                    return;
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    const parser = new DOMParser();
+                    const htmlString = this.responseText;
+                    const doc = parser.parseFromString(htmlString, "text/html");
+                    const body = doc.documentElement.children[1];
+                    const parent = element.parentNode;
+                    if (!parent) {
+                        return;
+                    }
+                    while (body.children.length > 0) {
+                        const child = body.children[body.children.length - 1];
+                        parent.insertBefore(child, element.nextSibling);
+                    }
+                    parent.removeChild(element);
+                    includeHTML(callback);
+                } else {
+                    console.warn(`File ${file} Not Found`);
+                    const parent = element.parentNode;
+                    parent.removeChild(element);
+                    includeHTML(callback);
                 }
-                while (body.children.length > 0) {
-                    const child = body.children[body.children.length - 1];
-                    parent.insertBefore(child, element.nextSibling);
-                }
-                parent.removeChild(element);
-                includeHTML(callback);
             }
         }
         xhttp.open("GET", file, true);
