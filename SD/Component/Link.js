15986b27a4dd823e486fbfc2c17ec0c9c64b05a7
whoseJam <276707964@qq.com>
2024-09-24 18:17:17 +0800

    [work]线段树优化dp

--- SD/Component/Link.js
+++ SD/Component/Link.js
@@ -6,8 +6,9 @@ import { trim } from "@/Utility/Trim";
 
 let id = 0;
 
-export function Link(elem1, elem2, linkClass = Line, xloc1 = "cx", yloc1 = "cy", xloc2 = "cx", yloc2 = "cy") {
+export function Link(elem1, elem2, linkClass = Line, xloc1 = "cx", yloc1 = "cy", xloc2 = "cx", yloc2 = "cy", callback = () => {}) {
     const link = new linkClass(svg());
+    callback(link);
     const move = function() {
         const x1 = elem1[xloc1](), y1 = elem1[yloc1]();
         const x2 = elem2[xloc2](), y2 = elem2[yloc2]();
