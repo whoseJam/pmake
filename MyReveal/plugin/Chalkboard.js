1995467b430f0999f5b7d2e8634d73995be79714
whoseJam <276707964@qq.com>
2024-10-07 15:32:58 +0800

    [fix]避免chalkboard无图报错

--- MyReveal/plugin/Chalkboard.js
+++ MyReveal/plugin/Chalkboard.js
@@ -243,7 +243,7 @@ const initChalkboard = function(Reveal) {
         if (config.theme) theme = config.theme;
         switch (theme) {
             case 'whiteboard':
-                background = ['rgba(127,127,127,.1)', path + 'img/whiteboard.png'];
+                background = ['rgba(127,127,127,.1)', ''];
                 draw = [drawWithBoardmarker, drawWithBoardmarker];
                 pens = [boardmarkers, boardmarkers];
                 grid = {
@@ -254,7 +254,7 @@ const initChalkboard = function(Reveal) {
                 break;
             case 'chalkboard':
             default:
-                background = ['rgba(127,127,127,.1)', path + 'img/blackboard.png'];
+                background = ['rgba(127,127,127,.1)', ''];
                 draw = [drawWithBoardmarker, drawWithChalk];
                 pens = [boardmarkers, chalks];
                 grid = {
