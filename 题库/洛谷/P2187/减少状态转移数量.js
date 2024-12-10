4e49f0861582837fdf4379cda734112f7e349990
whoseJam <276707964@qq.com>
2024-12-10 15:38:14 +0800

    [work]P2187

---
+++ 题库/洛谷/P2187/减少状态转移数量.js
@@ -0,0 +1,30 @@
+import * as sd from "@/sd";
+
+const svg = sd.svg();
+const C = sd.color();
+const n = 10;
+const i = 7;
+const str = " accbabbaac"
+const banned = "c";
+const arr = new sd.Array(svg).resize(n).start(1);
+
+sd.init(() => {
+    sd.Pointer(arr, "i", "b", 3, 20, 3).moveTo(i);
+    sd.Brace(arr).brace(1, i, "b").value("合法");
+})
+
+sd.main(async () => {
+    await sd.pause();
+    for (let j = 1; j < i; j++) {
+        sd.Link(arr.element(j), arr.element(i), sd.Curve, "cx", "y", "cx", "y").bending(-0.5).startAnimate().pointStoT().endAnimate().arrow();
+    }
+    await sd.pause();
+    arr.startAnimate();
+    for (let j = 1; j <= n; j++) arr.value(j, str[j]);
+    arr.endAnimate();
+
+    await sd.pause();
+    arr.startAnimate();
+    for (let j = 1; j <= i; j++) arr.color(j, (str[j] === banned) ? C.red : C.green);
+    arr.endAnimate();
+})
---
+++ 题库/洛谷/P2187/状态转移.js
@@ -0,0 +1,22 @@
+import * as sd from "@/sd";
+
+const svg = sd.svg();
+const C = sd.color();
+const n = 10;
+const i = 7;
+const j = 4;
+const arr = new sd.Array(svg).resize(n).start(1);
+
+sd.init(() => {
+    sd.Pointer(arr, "i", "b", 3, 20, 3).moveTo(i);
+    sd.Brace(arr).brace(1, i, "b").value("合法");
+})
+
+sd.main(async () => {
+    await sd.pause();
+    sd.Pointer(arr, "j", "b", 3, 20, 3).startAnimate().moveTo(j).endAnimate();
+    await sd.pause();
+    sd.Link(arr.element(j), arr.element(i), sd.Curve, "cx", "y", "cx", "y").bending(-0.5).startAnimate().pointStoT().endAnimate().arrow();
+    await sd.pause();
+    arr.startAnimate().color(j + 1, i - 1, C.red).endAnimate();
+})
