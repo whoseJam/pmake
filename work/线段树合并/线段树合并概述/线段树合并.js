a4ee2603445d6cf452febd85e4023005daa30a21
whoseJam <276707964@qq.com>
2024-12-19 22:13:09 +0800

    [work]线段树合并

--- work/线段树合并/线段树合并概述/线段树合并.js
+++ work/线段树合并/线段树合并概述/线段树合并.js
@@ -1,10 +1,13 @@
 import * as sd from "@/sd";
+import { MergeSegmentTree } from "../_/MergeSegmentTree";
 
 const svg = sd.svg();
 const C = sd.color();
 const tree1 = new sd.BinaryTree(svg).width(500);
 const tree2 = new sd.BinaryTree(svg).dy(250).width(500);
 const tree3 = new sd.BinaryTree(svg).dy(125).width(500).dx(500);
+const f1 = sd.Focus(tree1);
+const f2 = sd.Focus(tree2);
 const pushUpText = new sd.Mathjax(svg, "pushUp(u)").opacity(0);
 let tot = 0;
 
@@ -28,30 +31,27 @@ sd.init(() => {
 })
 
 sd.main(async () => {
-    await Merge(1, 8)
+    await MergeSegmentTree(tree1, tree2, {
+        OnFocusRoot: OnFocusRoot,
+        OnMergeLeftChild: OnMergeLeftChild,
+        OnFinishMergeLeftChild: OnFinishMergeLeftChild,
+        OnMergeRightChild: OnMergeRightChild,
+        OnFinishMergeRightChild: OnFinishMergeRightChild,
+        OnRemoveFocusRoot: OnRemoveFocusRoot
+    })
 })
 
-const f1 = sd.Focus(tree1);
-const f2 = sd.Focus(tree2);
-
-async function Merge(rt1, rt2) {
+async function OnFocusRoot(node1, node2, rt1) {
     await sd.pause();
-    f1.startAnimate().focus(rt1).endAnimate();
-    f2.startAnimate().focus(rt2).endAnimate();
-    if (rt1 === 1) {
-        tree3.startAnimate().root(rt1).endAnimate();
-    }
-    const lc1 = tree1.leftChildId(rt1);
-    const lc2 = tree2.leftChildId(rt2);
-    const rc1 = tree1.rightChildId(rt1);
-    const rc2 = tree2.rightChildId(rt2);
+    f1.startAnimate().focus(node1).endAnimate();
+    f2.startAnimate().focus(node2).endAnimate();
+    if (rt1 == 1) tree3.startAnimate().root(rt1).endAnimate();
+}
+
+async function OnMergeLeftChild(rt1, rt2, lc1, lc2) {
     if (lc1 && lc2) {
         await sd.pause();
         tree3.startAnimate().leftChild(rt1, lc1).endAnimate();
-        await Merge(lc1, lc2);
-        await sd.pause();
-        f1.startAnimate().focus(rt1).endAnimate();
-        f2.startAnimate().focus(rt2).endAnimate();
     } else {
         await sd.pause();
         const id = lc1 ? lc1 : lc2;
@@ -59,13 +59,12 @@ async function Merge(rt1, rt2) {
         tree3.startAnimate().leftChild(rt1, id).color(id, C.green).endAnimate();
         Build(tree, id, C.green);
     }
+}
+
+async function OnMergeRightChild(rt1, rt2, rc1, rc2) {
     if (rc1 && rc2) {
         await sd.pause();
         tree3.startAnimate().rightChild(rt1, rc1).endAnimate();
-        await Merge(rc1, rc2);
-        await sd.pause();
-        f1.startAnimate().focus(rt1).endAnimate();
-        f2.startAnimate().focus(rt2).endAnimate();
     } else {
         await sd.pause();
         const id = rc1 ? rc1 : rc2;
@@ -73,6 +72,25 @@ async function Merge(rt1, rt2) {
         tree3.startAnimate().rightChild(rt1, id).color(id, C.blue).endAnimate();
         Build(tree, id, C.blue);
     }
+}
+
+async function OnFinishMergeLeftChild(rt1, rt2, lc1, lc2) {
+    if (lc1 && lc2) {
+        await sd.pause();
+        f1.startAnimate().focus(rt1).endAnimate();
+        f2.startAnimate().focus(rt2).endAnimate();
+    }
+}
+
+async function OnFinishMergeRightChild(rt1, rt2, rc1, rc2) {
+    if (rc1 && rc2) {
+        await sd.pause();
+        f1.startAnimate().focus(rt1).endAnimate();
+        f2.startAnimate().focus(rt2).endAnimate();
+    }
+}
+
+async function OnRemoveFocusRoot(node1, node2, rt1, rt2) {
     await sd.pause();
     pushUpText.x(tree3.element(rt1).mx() + 5).cy(tree3.element(rt1).cy());
     pushUpText.startAnimate().opacity(1).endAnimate();
---
+++ work/线段树合并/雨天的尾巴/样例解释.js
@@ -0,0 +1,4 @@
+// import * as sd from "@/sd";
+
+// const svg = sd.svg();
+// const 
