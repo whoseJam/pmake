427cdda73a5b5801d21d3444f6a8474739142df1
whoseJam <276707964@qq.com>
2024-12-26 21:02:59 +0800

    [work]二分图

--- work/新二分图/二分图最大匹配/二分图最大匹配概述/匹配与最大匹配.js
+++ work/新二分图/二分图最大匹配/二分图最大匹配概述/匹配与最大匹配.js
@@ -18,7 +18,7 @@ sd.init(() => {
     for (let i = 1; i <= h; i++) bi.newNode(i, 0);
     for (let i = h + 1; i <= n; i++) bi.newNode(i, 1);
     links.forEach(link => bi.link(link[0], link[1]));
-    sd.Label(bi.element(1), "人", "lc", 20, 20);
+    sd.Label(bi.element(1), "乘客", "lc", 20, 20);
     sd.Label(bi.element(h + 1), "座位", "lc", 20, 20);
 })
 
@@ -26,7 +26,6 @@ sd.main(async () => {
     await Match([[3, 5], [2, 8]]);
     await Match([[3, 5], [4, 8]]);
     await Match([[1, 5], [3, 6], [4, 8]]);
-    
 })
 
 async function Match(matches) {
