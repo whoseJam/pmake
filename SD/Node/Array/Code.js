562eb13e34952c6a8077894110bc062af2efa7ec
whoseJam <276707964@qq.com>
2025-01-07 11:11:46 +0800

    [feat]迁移code

--- SD/Node/Array/Code.js
+++ SD/Node/Array/Code.js
@@ -1,38 +1,57 @@
 import { Context } from "@/Animate/Context";
 import { BaseArray } from "@/Node/Array/BaseArray";
 import { Rect } from "@/Node/Nake/Rect";
-import { SDNode } from "@/Node/SDNode";
-import { Enter } from "@/Node/SDNode/Enter";
+import { Enter as EN } from "@/Node/SDNode/Enter";
+import { effect } from "@/Node/SDNode/SDValue";
 import { Cast } from "@/Utility/Cast";
+import { Check } from "@/Utility/Check";
 import { Color } from "@/Utility/Color";
+import { Factory } from "@/Utility/Factory";
+
+function focusRule(parent, child) {
+    if (typeof parent.l() !== "number") return;
+    const l = parent.element(parent.l());
+    const r = parent.element(parent.r());
+    child.x(l.x()).y(l.y());
+    child.width(parent.width());
+    child.height(r.my() - l.y());
+}
 
 export function Code(parent, source = undefined) {
     BaseArray.call(this, parent);
 
     this.type("Code");
 
-    this.member.new("l", null);
-    this.member.new("r", null);
-    this.member.new("x", 0);
-    this.member.new("y", 0);
-    this.member.new("width", 0);
-    this.member.new("height", 0);
-    this.member.new("fontSize", 20);
-    this.member.set("start", 1);
+    this.vars.merge({
+        l: undefined,
+        r: undefined,
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        fontSize: 20,
+        start: 1,
+    });
 
-    this.childAs(
-        "focus",
-        new Rect(this).color(Color.BLUE).opacity(0),
-        (parent, child) => {
-            if (typeof (parent.l()) !== "number") return;
-            const elementL = parent.element(parent.l());
-            const elementR = parent.element(parent.r());
-            child.x(elementL.x());
-            child.y(elementL.y());
-            child.width(parent.width());
-            child.height(elementR.my() - elementL.y());
+    this._.updater = effect(() => {
+        const x = this.x();
+        let y = this.y();
+        let width = 0;
+        let height = 0;
+        const fontSize = this.fontSize();
+        const elements = this.vars.elements;
+        for (let element of elements) {
+            element.fontSize(fontSize);
+            element.x(x).y(y);
+            y += element.height();
+            width = Math.max(width, element.width());
+            height += element.height();
         }
-    );
+        this.vars.width = width;
+        this.vars.height = height;
+    });
+
+    this.childAs("focus", new Rect(this).color(Color.BLUE).opacity(0), focusRule);
 
     this.newLayer("elements");
 
@@ -40,44 +59,34 @@ export function Code(parent, source = undefined) {
 }
 
 Code.prototype = {
-    ...BaseArray.prototype
+    ...BaseArray.prototype,
 };
 
-Code.prototype.fontSize = SDNode.OrdinaryGSet("fontSize", "setByEqual");
-
-Code.prototype.updateList = [
-    ...Code.prototype.updateList,
-    update
-];
+Code.prototype.fontSize = Factory.handler("fontSize");
 
 Code.prototype.width = function (width) {
-    if (width === undefined) {
-        return this.member.get("width");
-    }
-    const k = width / this.member.get("width");
+    if (width === undefined) return this.vars.width;
+    const k = width / this.vars.width;
     this.fontSize(this.fontSize() * k);
     return this;
-}
+};
 
 Code.prototype.height = function (height) {
-    if (height === undefined) {
-        return this.member.get("height");
-    }
-    const k = height / this.member.get("height");
+    if (height === undefined) return this.vars.height;
+    const k = height / this.vars.height;
     this.fontSize(this.fontSize() * k);
     return this;
-}
+};
 
 Code.prototype.insert = function (index, value = "") {
     const element = Cast.castToSDNode(this.layer("elements"), value);
-    element.onEnter(Enter.ordinary(this, "elements"));
+    element.onEnter(EN.appear("elements"));
     this.insertByBaseArray(index, element);
     return this;
-}
+};
 
 Code.prototype.code = function (source) {
-    for (let i = this.end(); i >= this.start(); i--)
-        this.erase(i);
+    for (let i = this.end(); i >= this.start(); i--) this.erase(i);
     let ans = "";
     for (let i = 0; i < source.length; i++) {
         if (source[i] === "\n") {
@@ -87,67 +96,42 @@ Code.prototype.code = function (source) {
     }
     if (ans.length > 0) this.push(ans);
     return this;
-}
+};
 
 Code.prototype.focus = function (l, r) {
     const focus = this.child("focus");
-    if (l === null) {
-        this.member.setAndFlush("l", null);
-        this.member.setAndFlush("r", null);
+    if (Check.isFalseType(l)) {
+        this.freeze();
+        this.vars.l = undefined;
+        this.vars.r = undefined;
+        this.unfreeze();
         focus.opacity(0);
         return this;
-    } else if (arguments.length === 1) {
-        l = r = arguments[0];
-    }
-    this.member.setAndFlush("l", l);
-    this.member.setAndFlush("r", r);
-    if (!focus.opacity()) {
-        focus.onEnter((element, move) => {
-            const context = new Context(this);
-            element.startAnimate(context.tillc(0, 0));
-            move();
-            element.update();
-            element.startAnimate(context.tillc(0, 1));
-            element.opacity(1);
-        });
-    }
-    this.tryUpdate();
+    } else if (arguments.length === 1) return focus(arguments[0], arguments[1]);
+    const update = () => {
+        this.freeze();
+        this.vars.l = l;
+        this.vars.r = r;
+        this.unfreeze();
+    };
+    if (focus.opacity() === 0) {
+        const context = new Context(focus);
+        focus.startAnimate(context.tillc(0, 0));
+        update();
+        focus.startAnimate(context.tillc(0, 1));
+        focus.opacity(1);
+    } else update();
     return this;
-}
+};
 
 Code.prototype.l = function () {
-    return this.member.get("l");
-}
+    return this.vars.l;
+};
 
 Code.prototype.r = function () {
-    return this.member.get("r");
-}
+    return this.vars.r;
+};
 
 Code.prototype.value = function () {
     return this.element.apply(this, arguments);
-}
-
-function update() {
-    if (this.member.hasChanged("x") ||
-        this.member.hasChanged("y") ||
-        this.member.hasChanged("fontSize") ||
-        this.member.hasChanged("elements")) {
-        const x = this.x();
-        let y = this.y();
-        let width = 0;
-        let height = 0;
-        const fontSize = this.fontSize();
-        const elements = this.member.get("elements");
-        for (let element of elements) {
-            this.tryMove(element, () => {
-                element.fontSize(fontSize);
-                element.x(x).y(y);
-            });
-            y += element.height();
-            width = Math.max(width, element.width());
-            height += element.height();
-        }
-        this.member.setAndFlush("width", width);
-        this.member.setAndFlush("height", height);
-    }
-}
+};
