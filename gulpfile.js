ddbe850138a17d02423ac42553aafece50423956
whoseJam <276707964@qq.com>
2025-02-02 20:19:59 +0800

    [fix]一些兼容性问题

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp         = require("gulp");
-const aniTask      = require("./build/aniTask");
-const pptTask      = require("./build/pptTask");
+const gulp = require("gulp");
+const aniTask = require("./build/aniTask");
+const pptTask = require("./build/pptTask");
 
-const SDTask       = require("./build/SDTask");
+const SDTask = require("./build/SDTask");
 const MyRevealTask = require("./build/MyRevealTask");
 const colors = require("colors-console");
 
 let defaultConfig = undefined;
 try {
     defaultConfig = require("./myconfig.json");
-} catch(e) {
+} catch (e) {
     console.log(colors("red", "[error]未找到 myconfig.json 文件，请确保 ./pmake 目录下存在 myconfig.json 文件"));
-    console.log(colors("cyan", "myconfig.json") + " 需要配置如下：")
+    console.log(colors("cyan", "myconfig.json") + " 需要配置如下：");
     console.log(colors("cyan", "defaultAnimationTargetFilePath") + "：动画默认输出路径（例如 C:/Users/xxx/Desktop/output）");
     console.log(colors("cyan", "defaultPPTTargetFilePath") + "：ppt默认输出路径（例如 C:/Users/xxx/Desktop/output/animation）");
     process.exit(1);
 }
 
-const defaultAnimationTargetFilePath = defaultConfig["defaultAnimationTargetFilePath"];
-const defaultPPTTargetFilePath = defaultConfig["defaultPPTTargetFilePath"];
+const defaultAnimationTargetFilePath = defaultConfig["animationOutputPath"];
+const defaultPPTTargetFilePath = defaultConfig["pptOutputPath"];
 
 global["projectRoot"] = __dirname.replaceAll("\\", "/");
 
@@ -45,7 +45,7 @@ gulp.task("SD", () => {
 gulp.task("MyReveal", () => {
     parseInput();
     return MyRevealTask(defaultPPTTargetFilePath);
-})
+});
 
 gulp.task("ani", () => {
     parseInput();
@@ -54,11 +54,11 @@ gulp.task("ani", () => {
 
 gulp.task("animation", gulp.parallel("SD", "ani"));
 
-gulp.task("ppt", (done) => {
+gulp.task("ppt", done => {
     parseInput();
     const inputPath = global["i"];
     const outputPath = global["o"] ? global["o"] : defaultPPTTargetFilePath;
-    gulp.task("ppt-inner", function(done) {
+    gulp.task("ppt-inner", function (done) {
         pptTask(inputPath, outputPath);
         done();
     });
@@ -68,11 +68,11 @@ gulp.task("ppt", (done) => {
         return result;
     }
     return gulp.task("ppt-inner")(done);
-})
+});
 
-gulp.task("serve", (done) => {
+gulp.task("serve", done => {
     const exec = require("child_process").exec;
-    exec(`cd ${defaultPPTTargetFilePath} && live-server`, function(error, stdout, stderr) {
+    exec(`cd ${defaultPPTTargetFilePath} && live-server`, function (error, stdout, stderr) {
         if (error) {
             console.log(error);
         } else {
@@ -80,4 +80,4 @@ gulp.task("serve", (done) => {
         }
         done();
     });
-})
+});
