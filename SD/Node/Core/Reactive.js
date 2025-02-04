51bc44b786a3dc743cc6c562500c9e1587f83206
whoseJam <276707964@qq.com>
2025-02-04 13:03:08 +0800

    [feat]reactive DAG

--- SD/Node/Core/Reactive.js
+++ SD/Node/Core/Reactive.js
@@ -1,1 +1,1 @@
-export { checkEffect, effect, freeze, object, reactive, setPrecise, uneffect, unfreeze } from "@/Node/Core/Reactive_naive";
+export { checkEffect, effect, freeze, object, reactive, setPrecise, uneffect, unfreeze } from "@/Node/Core/Reactive_dag";
--- unit/reactive.js
+++ unit/reactive.js
@@ -3,11 +3,13 @@ import * as sd from "@/sd";
 const svg = sd.svg();
 
 sd.main(async () => {
-    // await TestChainImpact();
-    await TestCircleDependency();
+    await TestDAGUpdate();
     await TestCreateEffectInEffect();
-    await TestNestedRepeatDependency();
+    await TestVarsNoChange();
     await TestRepeatDependency();
+    await TestNestedRepeatDependency();
+    await TestCircleDependency();
+    await TestChainImpact();
 });
 
 async function TestCreateEffectInEffect() {
@@ -36,6 +38,48 @@ async function TestCreateEffectInEffect() {
     console.log("Test Create Effect In Effect Passed!");
 }
 
+async function TestDAGUpdate() {
+    let count = 0;
+    const a = new sd.SDNode(svg);
+    const b = new sd.SDNode(svg);
+    const c = new sd.SDNode(svg);
+    const d = new sd.SDNode(svg);
+    a.vars.merge({
+        x: 0,
+    });
+    b.vars.merge({
+        x: 0,
+    });
+    c.vars.merge({
+        x: 0,
+    });
+    d.vars.merge({
+        x: 0,
+    });
+    b.effect("e2", () => {
+        console.log("e2 triggered!");
+        b.vars.x = a.vars.x + 10;
+    });
+    c.effect("e3", () => {
+        console.log("e3 triggered!");
+        c.vars.x = a.vars.x + 20;
+    });
+    d.effect("e4", () => {
+        count++;
+        console.log("e4 triggered!");
+        d.vars.x = d.vars.x + b.vars.x;
+    });
+    d.effect("e5", () => {
+        console.log("e5 triggered!");
+        d.vars.x = c.vars.x * 2;
+    });
+    console.assert(d.vars.x === 50, "Effect initialize failed!");
+    console.assert(count === 2);
+    a.vars.x = 20;
+    console.assert(d.vars.x === 110, "Effect trigger failed!");
+    console.assert(count === 3, "Fail to optimize!");
+}
+
 async function TestVarsNoChange() {
     let count = 0;
     const a = new sd.SDNode(svg);
@@ -58,7 +102,9 @@ async function TestVarsNoChange() {
         b.vars.x = a.vars.x + 10;
     });
     a.vars.flag = 50;
-    console.assert(count === 1, "Failed to optimize!");
+    console.log("count=", count);
+    console.assert(count === 1, "Fail to optimize!");
+    console.log("Optimize Vars No Change Succeeded!");
 }
 
 async function TestNestedRepeatDependency() {
@@ -95,6 +141,7 @@ async function TestNestedRepeatDependency() {
     a.vars.x = 10;
     console.assert(b.vars.x === 0 && b.vars.y === 0, "Effect trigger error!");
     a.vars.flag1 = true;
+    console.log(b.vars.x, b.vars.y);
     console.assert(b.vars.x === 20 && b.vars.y === 0, "Effect trigger failed!");
     console.log("Test Nested Repeat Dependency Passed!");
 }
@@ -120,8 +167,7 @@ async function TestRepeatDependency() {
         b.vars.x = a.vars.x;
         b.vars.y = a.vars.y;
     });
-    console.assert(b.vars.x === 0, "Effect initialize failed!");
-    console.assert(b.vars.y === 0, "Effect initialize failed!");
+    console.assert(b.vars.x === 0 && b.vars.y === 0, "Effect initialize failed!");
     a.vars.x = 20;
     console.assert(b.vars.x === 40, "Effect trigger failed!");
     console.log("Test Repeat Dependency Passed!");
