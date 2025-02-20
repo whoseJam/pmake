2c8015a0e4e055fa8bfe134b6b50405ef568c304
whosejam <276707964@qq.com>
2025-02-20 16:56:00 +0800

    [feat]coord

--- unit/coord.js
+++ unit/coord.js
@@ -1,7 +1,7 @@
 import * as sd from "@/sd";
 
 const svg = sd.svg();
-const coord = new sd.Coord(svg).viewX(-2.5).viewY(-2.5).viewWidth(5).viewHeight(5);
+const coord = new sd.Coord(svg).viewBox(-2.5, -2.5, 5, 5);
 
 sd.init(() => {});
 
@@ -9,10 +9,14 @@ sd.main(async () => {
     await sd.pause();
     coord.startAnimate().x(100).y(100).endAnimate();
     await sd.pause();
-    coord
-        .startAnimate()
-        .draw(1, x => x * x)
-        .endAnimate();
+    coord.startAnimate()
+    const func1 = coord.draw(x => x * x)
+    coord.endAnimate();
+    coord.startAnimate();
+    const func2 = coord.draw(x => -x * x);
+    coord.endAnimate();
     await sd.pause();
     coord.startAnimate().viewX(-5).endAnimate();
+    await sd.pause();
+    coord.startAnimate().width(300).height(300).endAnimate();
 });
