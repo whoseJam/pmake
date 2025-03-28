9df7292dd8798c273db45d112d33994bd37d8046
whosejam <276707964@qq.com>
2025-03-28 15:31:07 +0800

    [work]概率与期望

--- work/概率与期望/跳一跳/样例解释.js
+++ work/概率与期望/跳一跳/样例解释.js
@@ -1,6 +1,7 @@
 import * as sd from "@/sd";
 
 const svg = sd.svg();
+const C = sd.color();
 const boxes = new sd.ValueArray(svg).elementWidth(60).start(1);
 const data = [1, 2, 1, 1, 3, 2, 1, 3];
 const n = data.length + 1;
@@ -12,27 +13,33 @@ sd.init(() => {
 });
 
 sd.main(async () => {
+    let cnt = 2;
     for (let i = 2; i <= n; i++) {
         await sd.pause();
+        const color = data[i - 2] === 1 ? C.textBlue : C.red;
         sd.Link(boxes.element(i - 1), boxes.element(i), sd.Curve, "cx", "y", "cx", "y")
             .bending(-0.5)
+            .stroke(color)
             .startAnimate()
             .pointStoT()
             .endAnimate()
             .arrow();
         if (data[i - 2] === 1) {
-            Appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, "+1"), "bc", 10));
+            appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, "+1"), "bc", 10));
+            cnt = 2;
         }
         if (data[i - 2] === 2) {
-            Appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, "+cnt"), "bc", 30));
+            appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, `+cnt(${cnt})`), "bc", 30));
+            cnt *= 2;
         }
         if (data[i - 2] === 3) {
-            Appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, "+cnt"), "bc", 30));
-            Appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, "+y_i"), "bc", 50));
+            appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, `+cnt(${cnt})`), "bc", 30));
+            appear(sd.Aside(boxes.element(i), new sd.Mathjax(svg, "+y_i"), "bc", 50));
+            cnt *= 2;
         }
     }
 });
 
-function Appear(element) {
-    element.opacity(0).after(300).startAnimate().opacity(1).endAnimate();
+function appear(element) {
+    element.fontSize(10).opacity(0).after(300).startAnimate().opacity(1).endAnimate();
 }
