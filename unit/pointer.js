697c584148bdfde69a6b7196c57a38511671eb7f
whoseJam <276707964@qq.com>
2025-04-04 15:02:45 +0800

    [fix]fix the error: [0,0] pointer moveTo doesn't show the pointer

--- unit/pointer.js
+++ unit/pointer.js
@@ -1,12 +1,23 @@
 import * as sd from "@/sd";
 
 const svg = sd.svg();
-const arr = new sd.Array(svg).x(100).y(100).resize(10);
-const tree = new sd.Tree(svg).x(500).y(100).root(1).link(1, 2).link(1, 3);
 
 sd.init(() => {});
 
-sd.main(async () => {
+sd.main(TestWithArray);
+
+async function TestWithArray() {
+    const svg = sd.svg();
+    const n = 7;
+    const arr = new sd.Array(svg).x(100).y(100).resize(n);
+    sd.Pointer(arr, "start", "b").moveTo(arr.start());
+    sd.Pointer(arr, "end", "b").moveTo(arr.end());
+    sd.Brace(arr).brace(arr.start(), arr.end(), "b").value("length");
+}
+
+async function TestBasic() {
+    const arr = new sd.Array(svg).x(100).y(100).resize(10);
+    const tree = new sd.Tree(svg).x(500).y(100).root(1).link(1, 2).link(1, 3);
     const p = sd.Pointer(arr, "Pointer", "b");
     for (let i = arr.start(); i <= arr.end(); i++) {
         await sd.pause();
@@ -41,4 +52,4 @@ sd.main(async () => {
     arr.startAnimate();
     pB.moveTo(tree.element(1));
     arr.endAnimate();
-});
+}
