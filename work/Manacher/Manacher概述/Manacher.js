d8fa2adcff32037626bb4b28896c03d60ab7e410
whosejam <276707964@qq.com>
2025-04-18 22:38:50 +0800

    [work]Manacher

--- work/Manacher/Manacher概述/Manacher.js
+++ work/Manacher/Manacher概述/Manacher.js
@@ -5,54 +5,64 @@ const svg = sd.svg();
 // const data = "abbabbabaaaba";
 const data = "aaaaabaa";
 const str = new sd.Array(svg);
-const pos = sd.Pointer(str, "pos", "b", 15, 20, 3);
-const idx = sd.Pointer(str, "i", "b", 15, 40, 3);
+const pos = sd.Pointer(str, "pos", "b", 15, 20);
+const idx = sd.Pointer(str, "i", "b", 15, 20);
 const mirror = sd.Brace(str);
 const mirrorCenter = sd.Brace(str);
 const current = sd.Brace(str);
 
+let currentI = 0;
+
 sd.init(() => {
     str.pushArray(data);
     str.cx(600).cy(300);
-})
+});
 
 sd.main(async () => {
     await Manacher(str, {
-        OnIMoveTo: OnIMoveTo,
-        OnCheckMirrorSymmetry: OnCheckMirrorSymmetry,
-        OnILengthInitialized: OnILengthUpdated,
-        OnILengthExtended: OnILengthUpdated,
-        OnMirrorSymmetryCenterUpdated: OnMirrorSymmetryCenterUpdated,
-        OnILengthCalcFinished: async () => {
+        onIMove,
+        onCheckMirrorSymmetry,
+        onILengthInitialized: onILengthUpdated,
+        onILengthExtended: onILengthUpdated,
+        onMirrorSymmetryCenterUpdated,
+        onILengthCalcFinished: async () => {
             await sd.pause();
             mirror.startAnimate().opacity(0).endAnimate();
             current.startAnimate().opacity(0).endAnimate();
-        }
+        },
     });
-})
+});
 
-async function OnIMoveTo(i) {
+async function onIMove(i) {
     await sd.pause();
+    currentI = i;
     str.startAnimate();
-    idx.moveTo(i);
+    idx.moveTo(i).length(20);
     str.endAnimate();
 }
 
-async function OnCheckMirrorSymmetry(center, length) {
+async function onCheckMirrorSymmetry(center, length) {
     await sd.pause();
-    mirror.startAnimate().brace(center - length + 1, center + length - 1, "b", 5).endAnimate();
+    mirror
+        .startAnimate()
+        .brace(center - length + 1, center + length - 1, "b", 5)
+        .endAnimate();
     current.after(mirror).brace(center - length + 1, center + length - 1, "b", 5);
 }
 
-async function OnILengthUpdated(i, length) {
+async function onILengthUpdated(i, length) {
     await sd.pause();
-    current.startAnimate().brace(i - length + 1, i + length - 1, "b", 15).endAnimate();
+    current
+        .startAnimate()
+        .brace(i - length + 1, i + length - 1, "b", 15)
+        .endAnimate();
 }
 
-async function OnMirrorSymmetryCenterUpdated(center, length) {
+async function onMirrorSymmetryCenterUpdated(center, length) {
     await sd.pause();
     str.startAnimate();
     pos.moveTo(center);
+    if (center === currentI) idx.length(40);
     mirrorCenter.brace(center - length + 1, center + length - 1);
     str.endAnimate();
 }
--- work/Manacher/_/Manacher.js
+++ work/Manacher/_/Manacher.js
@@ -1,32 +1,30 @@
 import * as sd from "@/sd";
 
-
 /**
- * @param {sd.Array} str 
+ * @param {sd.Array} str
  * @param {{
- *  OnIMoveTo: (i: number) => void
- *  OnCheckMirrorSymmetry: (center: number, length: number) => void
- *  OnILengthInitialized: (i: number, length: number) => void
- *  OnILengthExtended: (i: number, length: number) => void
- *  OnILengthCalcFinished: (i: number, length: number) => void
- *  OnMirrorSymmetryCenterUpdated: (center: number, length: number) => void
+ *  onIMove: (i: number) => void
+ *  onCheckMirrorSymmetry: (center: number, length: number) => void
+ *  onILengthInitialized: (i: number, length: number) => void
+ *  onILengthExtended: (i: number, length: number) => void
+ *  onILengthCalcFinished: (i: number, length: number) => void
+ *  onMirrorSymmetryCenterUpdated: (center: number, length: number) => void
  * }} args
  */
 export async function Manacher(str, args) {
-    const OnIMoveTo = args.OnIMoveTo;
-    const OnCheckMirrorSymmetry = args.OnCheckMirrorSymmetry;
-    const OnILengthInitialized = args.OnILengthInitialized;
-    const OnILengthExtended = args.OnILengthExtended;
-    const OnILengthCalcFinished = args.OnILengthCalcFinished;
-    const OnMirrorSymmetryCenterUpdated = args.OnMirrorSymmetryCenterUpdated;
+    const onIMove = args.onIMove;
+    const onCheckMirrorSymmetry = args.onCheckMirrorSymmetry;
+    const onILengthInitialized = args.onILengthInitialized;
+    const onILengthExtended = args.onILengthExtended;
+    const onILengthCalcFinished = args.onILengthCalcFinished;
+    const onMirrorSymmetryCenterUpdated = args.onMirrorSymmetryCenterUpdated;
 
     const len = sd.make1d(str.length() * 5, 1);
 
     await sd.pause();
     const cx = str.cx();
     let data = "";
-    for (let i = str.start(); i <= str.end(); i++)
-        data = data + str.text(i);
+    for (let i = str.start(); i <= str.end(); i++) data = data + str.text(i);
     str.startAnimate();
     str.freeze();
     str.insert(0, "{");
@@ -38,40 +36,29 @@ export async function Manacher(str, args) {
     str.unfreeze();
     str.endAnimate();
 
-    let max = 0, pos = 0;
+    let max = 0,
+        pos = 0;
     for (let i = str.start() + 1; i <= str.end() - 1; i++) {
-        if (OnIMoveTo) {
-            await OnIMoveTo(i);
-        }
+        if (onIMove) await onIMove(i);
 
         if (max > i) {
-            if (OnCheckMirrorSymmetry) {
-                await OnCheckMirrorSymmetry(pos * 2 - i, len[pos * 2 - i]);
-            }
+            if (onCheckMirrorSymmetry) await onCheckMirrorSymmetry(pos * 2 - i, len[pos * 2 - i]);
             len[i] = Math.min(len[pos * 2 - i], max - i);
         } else len[i] = 1;
-        
-        if (OnILengthInitialized) {
-            await OnILengthInitialized(i, len[i]);
-        }
+
+        if (onILengthInitialized) await onILengthInitialized(i, len[i]);
 
         while (i + len[i] <= str.end() && i - len[i] >= str.start() && str.text(i + len[i]) == str.text(i - len[i])) {
             len[i]++;
-            if (OnILengthExtended) {
-                await OnILengthExtended(i, len[i]);
-            }
+            if (onILengthExtended) await onILengthExtended(i, len[i]);
         }
 
         if (max < i + len[i]) {
             max = i + len[i];
             pos = i;
-            if (OnMirrorSymmetryCenterUpdated) {
-                await OnMirrorSymmetryCenterUpdated(i, len[i]);
-            }
+            if (onMirrorSymmetryCenterUpdated) await onMirrorSymmetryCenterUpdated(i, len[i]);
         }
 
-        if (OnILengthCalcFinished) {
-            await OnILengthCalcFinished(i, len[i]);
-        }
+        if (onILengthCalcFinished) await onILengthCalcFinished(i, len[i]);
     }
 }
