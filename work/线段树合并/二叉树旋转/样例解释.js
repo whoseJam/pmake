f4093791658d00159d67d3d8d559b06e6d092fc9
whoseJam <276707964@qq.com>
2025-05-10 16:56:48 +0800

    [work]线段树合并

--- work/线段树合并/二叉树旋转/样例解释.js
+++ work/线段树合并/二叉树旋转/样例解释.js
@@ -6,13 +6,15 @@ const C = sd.color();
 const R = sd.rule();
 const tree = new sd.BinaryTree(svg).x(100).width(400).layerHeight(80);
 const arr = new sd.Array(svg);
-const n = 5;
-const permutation = [4, 2, 1, 3, 5];
+const n = 4;
+const permutation = [0, 0, 0, 4, 2, 1, 3];
 const links = [
     [1, 2],
     [1, 3],
     [3, 4],
     [3, 5],
+    [2, 6],
+    [2, 7],
 ];
 
 sd.init(() => {
@@ -40,7 +42,8 @@ sd.main(async () => {
     tree.forEachNode(node => node.onClick(null));
     arr.x(tree.cx() - (arr.elementWidth() * n) / 2).y(tree.my() + 40);
     tree.forEachNodeOnPreorderTraversal(node => {
-        arr.startAnimate().push(node.intValue()).endAnimate();
+        const i = node.intValue();
+        if (i) arr.startAnimate().push(i).endAnimate();
     });
     await sd.pause();
     for (let i = 0; i < arr.length(); i++) for (let j = i + 1; j < arr.length(); j++) if (arr.intValue(i) > arr.intValue(j)) sd.Link(arr.element(i), arr.element(j), sd.Curve, "cx", "my", "cx", "my").bending(0.3).startAnimate().pointStoT().endAnimate();
