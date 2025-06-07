08341e95efc53422b060b6e5776918c2064df9f1
whoseJam <276707964@qq.com>
2025-06-07 11:55:45 +0800

    [work]网络流 & 线段树分治

--- work/网络流/最小割/最大获利/建图.js
+++ work/网络流/最小割/最大获利/建图.js
@@ -4,18 +4,18 @@ const svg = sd.svg();
 const graph = new sd.GridGraph(svg).height(200).width(500).cx(600).cy(300);
 const R = sd.rule();
 const links = [
-    { from: "S", to: "中转1", cap: "P_1", xloc: "mx", yloc: "my" },
-    { from: "中转1", to: "用户1", cap: "\\infty", xloc: "mx", yloc: "my" },
-    { from: "中转1", to: "用户2", cap: "\\infty", xloc: "mx", yloc: "y" },
-    { from: "用户1", to: "T", cap: "C_1", xloc: "x", yloc: "my" },
-    { from: "用户2", to: "T", cap: "C_2", xloc: "x", yloc: "y" },
+    { from: "S", to: "用户1", cap: "C_1", xloc: "mx", yloc: "my" },
+    { from: "用户1", to: "中转1", cap: "\\infty", xloc: "mx", yloc: "my" },
+    { from: "用户1", to: "中转2", cap: "\\infty", xloc: "mx", yloc: "y" },
+    { from: "中转1", to: "T", cap: "P_1", xloc: "x", yloc: "my" },
+    { from: "中转2", to: "T", cap: "P_2", xloc: "x", yloc: "y" },
 ];
 
 sd.init(() => {
     graph.at(0.5, 0).newNode("S");
-    graph.at(0.5, 0.33).newNode("中转1");
-    graph.at(0, 0.66).newNode("用户1");
-    graph.at(1, 0.66).newNode("用户2");
+    graph.at(0.5, 0.33).newNode("用户1");
+    graph.at(0, 0.66).newNode("中转1");
+    graph.at(1, 0.66).newNode("中转2");
     graph.at(0.5, 1).newNode("T");
     links.forEach(link => {
         graph.link(link.from, link.to);
