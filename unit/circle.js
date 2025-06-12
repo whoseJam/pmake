1daa44a1032b34bf676dcb937b7275a76bc2b8e6
whoseJam <276707964@qq.com>
2025-06-12 16:58:05 +0800

    [work]构造题

--- unit/circle.js
+++ unit/circle.js
@@ -7,14 +7,22 @@ const C = sd.color();
 sd.main(TestCircleHTMLAndSVG);
 
 async function TestCircleHTMLAndSVG() {
+    console.log("hello 1");
     const c1 = new sd.Circle(div).cx(600).cy(300);
     const c2 = new sd.Circle(svg).cx(700).cy(300);
     await sd.pause();
+    console.log("hello 2");
     c1.startAnimate().r(40).cx(600).cy(300).endAnimate();
     c2.startAnimate().r(40).endAnimate();
     await sd.pause();
+    console.log("hello 3");
     c1.startAnimate().x(100).y(100).endAnimate();
     c2.startAnimate().x(200).y(100).endAnimate();
+    for (let i = 1; i <= 30; i++) {
+        await sd.pause();
+        console.log(i);
+    }
+    throw new Error("I come here");
 }
 
 async function TestBasic() {
