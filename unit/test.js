b5d8052c59ff7a2d048f75315c7dbc3bfd65bfc7
whoseJam <276707964@qq.com>
2025-06-23 19:18:16 +0800

    [feat]support cross origin message

--- unit/test.js
+++ unit/test.js
@@ -1,94 +1,54 @@
 import * as sd from "@/sd";
 
+const gridSize = 20;
+
 const svg = sd.svg();
 const C = sd.color();
-
 const grid = new sd.Grid(svg);
+grid.n(gridSize).m(gridSize);
 
-const n = 6,
-    m = 6;
-const horseX = 3,
-    horseY = 3;
+const startX = 0;
+const startY = 0;
+const endX = 6;
+const endY = 6;
+const horseX = 3;
+const horseY = 3;
 
-const horseControl = [
-    [0, 0],
-    [-2, -1],
-    [-2, 1],
-    [-1, -2],
-    [-1, 2],
-    [1, -2],
-    [1, 2],
-    [2, -1],
-    [2, 1],
+const horseControlPoints = [
+    [horseX + 1, horseY + 2],
+    [horseX + 2, horseY + 1],
+    [horseX + 2, horseY - 1],
+    [horseX + 1, horseY - 2],
+    [horseX - 1, horseY - 2],
+    [horseX - 2, horseY - 1],
+    [horseX - 2, horseY + 1],
+    [horseX - 1, horseY + 2],
 ];
 
-function isHorseControl(x, y) {
-    for (let [dx, dy] of horseControl) {
-        if (x === horseX + dx && y === horseY + dy) {
-            return true;
-        }
-    }
-    return false;
-}
-
-const dp = [];
-for (let i = 0; i <= n; i++) {
-    dp[i] = [];
-    for (let j = 0; j <= m; j++) {
-        dp[i][j] = 0;
-    }
-}
+const dp = Array.from({ length: gridSize }, () => Array.from({ length: gridSize }, () => 0));
 
 sd.init(() => {
-    grid.n(n + 1).m(m + 1);
-    grid.x(50).y(50);
-
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j <= m; j++) {
-            grid.text(i, j, "0");
-            if (isHorseControl(i, j)) {
-                grid.color(i, j, C.red);
-            } else {
-                grid.color(i, j, C.grey);
-            }
-        }
-    }
-
-    grid.text(0, 0, "1").color(0, 0, C.green);
-    dp[0][0] = 1;
+    grid.value(startX, startY, new sd.Text(svg, "1")).color(startX, startY, C.green);
+    horseControlPoints.forEach(([x, y]) => grid.value(x, y, new sd.Text("0")).color(x, y, C.red));
+    grid.value(horseX, horseY, new sd.Text(svg, "0")).color(horseX, horseY, C.green);
 });
 
 sd.main(async () => {
-    await sd.pause();
+    dp[startX][startY] = 1;
 
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j <= m; j++) {
-            if (i === 0 && j === 0) continue;
-
-            await sd.pause();
-
-            grid.startAnimate().color(i, j, C.yellow).endAnimate();
-
-            if (isHorseControl(i, j)) {
-                dp[i][j] = 0;
-                grid.startAnimate().text(i, j, "0").color(i, j, C.red).endAnimate();
-            } else {
-                let value = 0;
-                if (i > 0 && !isHorseControl(i - 1, j)) {
-                    value += dp[i - 1][j];
-                }
-                if (j > 0 && !isHorseControl(i, j - 1)) {
-                    value += dp[i][j - 1];
-                }
-                dp[i][j] = value;
-
-                grid.startAnimate().text(i, j, value.toString()).color(i, j, C.blue).endAnimate();
+    for (let i = 0; i <= endX; i++) {
+        for (let j = 0; j <= endY; j++) {
+            if ((i === startX && j === startY) || (i === horseX && j === horseY) || horseControlPoints.some(([x, y]) => x === i && y === j)) {
+                await sd.pause();
+                continue;
             }
+            if (i > 0 && dp[i - 1][j] > 0) dp[i][j] += dp[i - 1][j];
+            if (j > 0 && dp[i][j - 1] > 0) dp[i][j] += dp[i][j - 1];
 
-            await sd.pause();
+            if (dp[i][j] > 0) {
+                await sd.pause();
+                grid.value(i, j, new sd.Text(svg, dp[i][j].toString())).color(i, j, C.blue);
+            }
         }
     }
-
-    await sd.pause();
-    grid.startAnimate().color(n, m, C.green).endAnimate();
 });
