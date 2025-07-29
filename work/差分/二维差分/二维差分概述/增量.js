2ee69f8d0c934c1ab91008f86e73af8a1303a3e3
whoseJam <276707964@qq.com>
2025-07-29 14:50:54 +0800

    [work]二维差分

--- work/差分/二维差分/二维差分概述/增量.js
+++ work/差分/二维差分/二维差分概述/增量.js
@@ -2,13 +2,66 @@ import * as sd from "@/sd";
 
 const svg = sd.svg();
 const C = sd.color();
-const a = new sd.Grid(svg).n(3).m(4).startN(1).startM(1);
+const R = sd.rule();
+const n = 4;
+const m = 6;
+const a = new sd.Grid(svg).n(n).m(m).startN(1).startM(1);
+const focus = sd.Focus(a);
 
 sd.init(() => {
-    a.color(C.orange).color(3, 4, C.white);
+    sd.Label(a, "d");
+    a.forEachElement(element => {
+        element.childAs("cover", new sd.Rect(svg).color(C.orange).fillOpacity(0).strokeWidth(0), R.centerOnly());
+    });
 });
 
 sd.main(async () => {
     await sd.pause();
-    a.startAnimate().color(3, 4, C.yellow).endAnimate();
+    focus.startAnimate().focus(1, 1, n, m).endAnimate();
+    await sd.pause();
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            const element = a.element(i, j);
+            element.child("cover").startAnimate().fillOpacity(0.5).endAnimate();
+        }
+    }
+    await sd.pause();
+    focus
+        .startAnimate()
+        .focus(1, 1, n - 1, m)
+        .endAnimate();
+    await sd.pause();
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j <= m; j++) {
+            const element = a.element(i, j);
+            element.child("cover").startAnimate().fillOpacity(0).endAnimate();
+        }
+    }
+    await sd.pause();
+    focus
+        .startAnimate()
+        .focus(1, 1, n, m - 1)
+        .endAnimate();
+    await sd.pause();
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j < m; j++) {
+            const element = a.element(i, j);
+            if (i === n) element.child("cover").startAnimate().fillOpacity(0).endAnimate();
+            else element.child("cover").startAnimate().color(C.textBlue).fillOpacity(0.5).endAnimate();
+        }
+    }
+    await sd.pause();
+    focus
+        .startAnimate()
+        .focus(1, 1, n - 1, m - 1)
+        .endAnimate();
+    await sd.pause();
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < m; j++) {
+            const element = a.element(i, j);
+            element.child("cover").startAnimate().fillOpacity(0).endAnimate();
+        }
+    }
+    await sd.pause();
+    focus.startAnimate().focus(null).endAnimate();
 });
--- work/差分/二维差分/二维差分概述/差分对前缀和的影响.js
+++ work/差分/二维差分/二维差分概述/差分对前缀和的影响.js
@@ -4,23 +4,46 @@ const svg = sd.svg();
 const div = sd.div();
 const R = sd.rule();
 const n = 5;
-const d = new sd.Grid(svg).n(n).m(n);
-const a = new sd.Grid(svg).n(n).m(n).dx(400);
+const d = new sd.Grid(svg).elementWidth(75).elementHeight(75);
+const a = new sd.Grid(svg).elementWidth(75).elementHeight(75).dx(450);
 
 sd.init(() => {
+    for (let i = 0; i < n; i++)
+        for (let j = 0; j < n; j++) {
+            d.insert(i, j, 0);
+            a.insert(i, j, 0);
+        }
     sd.Label(a, "a", "tc");
     sd.Label(d, "d", "tc");
     d.forEachElement((element, i, j) => {
-        element.onClick(() => {
-            sd.inter(async () => {
-                await onAdd(i, j, +1);
-            });
-        });
-        element.onDblClick(() => {
-            sd.inter(async () => {
-                await onAdd(i, j, -1);
-            });
-        });
+        element.childAs(
+            new sd.Button(svg)
+                .text("+")
+                .width(20)
+                .height(30)
+                .onClick(() => {
+                    sd.inter(async () => {
+                        await onAdd(i, j, +1);
+                    });
+                }),
+            (parent, child) => {
+                child.x(parent.x()).y(parent.y());
+            }
+        );
+        element.childAs(
+            new sd.Button(svg)
+                .text("-")
+                .width(20)
+                .height(30)
+                .onClick(() => {
+                    sd.inter(async () => {
+                        await onAdd(i, j, -1);
+                    });
+                }),
+            (parent, child) => {
+                child.mx(parent.mx()).y(parent.y());
+            }
+        );
     });
 });
 
