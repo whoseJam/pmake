b00edbe418638652b64822d8069cbf13764ef0a3
whoseJam <276707964@qq.com>
2025-07-30 20:53:21 +0800

    [work]比赛

--- work/最小生成树/新的开始/建图.js
+++ work/最小生成树/新的开始/建图.js
@@ -2,7 +2,7 @@ import * as sd from "@/sd";
 
 const svg = sd.svg();
 const n = 6;
-const nodes = new sd.ValueArray(svg);
+const nodes = new sd.ValueArray(svg).elementWidth(80).start(1);
 
 sd.init(() => {
     for (let i = 1; i <= n; i++) nodes.push(new sd.Vertex(nodes, i));
@@ -12,24 +12,24 @@ sd.main(async () => {
     await sd.pause();
     for (let i = 1; i <= n; i++) {
         for (let j = i + 1; j <= n; j++) {
-            sd.Link(nodes[i], nodes[j], sd.Curve).startAnimate().pointStoT().endAnimate();
+            sd.Link(nodes.element(i), nodes.element(j), sd.Curve).bending(0.4).startAnimate().pointStoT().endAnimate();
         }
     }
     await sd.pause();
     const S = new sd.Vertex(svg, "S")
         .cx(nodes.cx())
-        .my(nodes.y() - 60)
+        .my(nodes.y() - 40)
         .opacity(0)
         .startAnimate()
         .opacity(1)
         .endAnimate();
     await sd.pause();
     for (let i = 1; i <= n; i++) {
-        sd.Link(S, nodes[i]).startAnimate().pointStoT().endAnimate();
+        sd.Link(S, nodes.element(i)).startAnimate().pointStoT().endAnimate();
     }
     new sd.Mathjax(svg, "v_i")
-        .x(nodes.x())
-        .cy((nodes.y() + S.my()) / 2)
+        .x(nodes.x() + 120)
+        .my((nodes.y() + S.my()) / 2)
         .opacity(0)
         .startAnimate()
         .opacity(1)
