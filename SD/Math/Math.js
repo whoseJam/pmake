32b04995dad23ff254a079555346de0aea209555
whoseJam <276707964@qq.com>
2025-08-04 10:01:55 +0800

    [work]Exgcd

--- SD/Math/Math.js
+++ SD/Math/Math.js
@@ -9,20 +9,18 @@ function dcmp(x) {
 }
 
 export function equal(x, y) {
-    if (typeof(x) !== "number" || typeof(y) !== "number") return false;
+    if (typeof x !== "number" || typeof y !== "number") return false;
     return dcmp(x - y) === 0;
 }
 
 export function dqual(x, y) {
-    if (typeof(x) !== "number" || typeof(y) !== "number") return false;
+    if (typeof x !== "number" || typeof y !== "number") return false;
     return ddcmp(x - y) === 0;
 }
 
 export function mapTo(left, length, newLeft, newLength) {
-    return function(k) {
-        if (length === 0) {
-            return newLeft;
-        }
-        return newLeft + (k - left) / length * newLength;
-    }
+    return function (k) {
+        if (length === 0) return newLeft;
+        return newLeft + ((k - left) / length) * newLength;
+    };
 }
