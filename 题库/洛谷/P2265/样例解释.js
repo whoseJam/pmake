0cda7e13e8912015c90a9f9acefdffe7bfe81b52
whoseJam <276707964@qq.com>
2025-08-06 19:48:05 +0800

    [work]考试

--- 题库/洛谷/P2265/样例解释.js
+++ 题库/洛谷/P2265/样例解释.js
@@ -2,7 +2,6 @@ import * as sd from "@/sd";
 
 const svg = sd.svg();
 const C = sd.color();
-const arr = new sd.Array(svg);
 const grid = new sd.Grid(svg);
 const n = 4;
 const m = 6;
@@ -11,12 +10,10 @@ sd.init(() => {
     grid.n(n + 1).m(m + 1);
     sd.Brace(grid, "l").brace(grid.element(0, 0), grid.element(n, 0)).value("n+1");
     sd.Brace(grid, "t").brace(grid.element(0, 0), grid.element(0, m)).value("m+1");
-    arr.resize(n + m);
-    arr.cx(grid.cx()).y(grid.my() + 50);
-    sd.Label(arr, "操作序列", "lc");
 });
 
 sd.main(async () => {
+    await sd.pause();
     let result = [];
     for (let i = 1; i <= n; i++) result.push("up");
     for (let i = 1; i <= m; i++) result.push("right");
@@ -25,19 +22,11 @@ sd.main(async () => {
         result[lst] = result[i];
         result[i] = tmp;
     }
-    await sd.pause();
-    arr.startAnimate();
-    for (let i = 0; i < n + m; i++) {
-        arr.value(i, result[i] === "up" ? "↑" : "→");
-    }
-    arr.endAnimate();
     grid.startAnimate().color(n, 0, C.blue).endAnimate();
 
     let curx = n;
     let cury = 0;
     for (let i = 0; i < n + m; i++) {
-        await sd.pause();
-        arr.startAnimate().color(i, C.blue).endAnimate();
         if (result[i] === "up") curx--;
         else cury++;
         grid.startAnimate().color(curx, cury, C.blue).endAnimate();
---
+++ 题库/洛谷/P9147/修改.js
@@ -0,0 +1,8 @@
+import * as sd from "@/sd";
+
+const svg = sd.svg();
+const bar = new sd.BarArray(svg).pushArray([1, 4, 2, 5, 3, 5, 7, 3, 5, 8, 6]);
+
+sd.init(() => {});
+
+sd.main(async () => {});
