cef5a458324a4711c1996f7c4b6a853e45eb6ed6
whoseJam <276707964@qq.com>
2025-08-12 09:41:47 +0800

    [work]概率与期望

--- work/Floyd/_/Floyd.js
+++ work/Floyd/_/Floyd.js
@@ -1,7 +1,7 @@
 import * as sd from "@/sd";
 
 /**
- * @param {sd.GraphBase} graph 
+ * @param {sd.GraphBase} graph
  */
 export async function floyd(graph) {
     const C = sd.color();
@@ -9,7 +9,7 @@ export async function floyd(graph) {
     const nodes = graph.nodes();
     const links = graph.links();
     const n = nodes.length;
-    const data = sd.make2d(n+1, n+1, Infinity);
+    const data = sd.make2d(n + 1, n + 1, Infinity);
     nodes.forEach((node, idx) => {
         node.idx = idx;
     });
@@ -17,39 +17,44 @@ export async function floyd(graph) {
     sd.Index(map, "t");
     sd.Index(map, "l");
     graph.childAs("map", map, R.aside("rc", 60));
-    links.forEach((link) => {
+    for (let i = 1; i <= n; i++) data[i][i] = 0;
+    links.forEach(link => {
         const x = +graph.sourceId(link);
         const y = +graph.targetId(link);
         data[x][y] = Math.min(data[x][y], +link.value().text());
     });
     for (let i = 1; i <= n; i++)
-        for (let j = 1; j <= n; j++)
-            if (i !== j) map.value(i, j, data[i][j]);
-            else map.value(i, j, 0);
+        for (let j = 1; j <= n; j++) {
+            const element = map.element(i, j);
+            const value = new sd.Mathjax(graph, data[i][j] === Infinity ? "$\\infty$" : data[i][j]);
+            element.value(value.fontSize(15), R.centerOnly());
+        }
 
     for (let k = 1; k <= n; k++) {
         const ek = graph.element(k);
         await sd.pause();
         graph.startAnimate().color(k, C.blue).endAnimate();
-        for (let i = 1; i <= n ; i++) {
+        for (let i = 1; i <= n; i++) {
             const ei = graph.element(i);
             for (let j = 1; j <= n; j++) {
                 const ej = graph.element(j);
-
-                const dij = map.intValue(i, j);
-                const dik = map.intValue(i, k);
-                const dkj = map.intValue(k, j);
+                const dij = data[i][j];
+                const dik = data[i][k];
+                const dkj = data[k][j];
                 if (dij > dik + dkj) {
                     await sd.pause();
                     const lij = sd.Link(ei, ej, sd.Curve).stroke(C.red).startAnimate().pointStoT().endAnimate().arrow();
                     const lik = sd.Link(ei, ek, sd.Curve).stroke(C.green).startAnimate().pointStoT().endAnimate().arrow();
                     const lkj = sd.Link(ek, ej, sd.Curve).stroke(C.green).startAnimate().pointStoT().endAnimate().arrow();
                     await sd.pause();
-                    map.startAnimate().value(i, j, dik + dkj).endAnimate();
+                    map.startAnimate()
+                        .text(i, j, dik + dkj)
+                        .endAnimate();
                     await sd.pause();
                     lij.startAnimate().fadeStoT().endAnimate().arrow(false).remove();
                     lik.startAnimate().fadeStoT().endAnimate().arrow(false).remove();
                     lkj.startAnimate().fadeStoT().endAnimate().arrow(false).remove();
+                    data[i][j] = dik + dkj;
                 }
             }
         }
--- work/Floyd/传递闭包概述/传递闭包.js
+++ work/Floyd/传递闭包概述/传递闭包.js
@@ -5,21 +5,18 @@ const graph = new sd.DAG(svg).width(200).height(200);
 
 sd.init(() => {
     graph.freeze();
-    for (let i = 1; i <= 6; i++) {
-        graph.newNode(i);
-    }
-    function link(a, b) {
-        graph.link(a, b);
-        graph.element(a, b).arrow();
-    }
+    for (let i = 1; i <= 6; i++) graph.newNode(i);
     link(1, 2);
     link(3, 2);
     link(2, 4);
     link(2, 5);
     link(3, 6);
     graph.unfreeze();
-})
+});
 
-sd.main(async () => {
+sd.main(async () => {});
 
-})
+function link(a, b) {
+    graph.link(a, b);
+    graph.element(a, b).arrow();
+}
