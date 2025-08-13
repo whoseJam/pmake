7c4f887de795033e432ea527406f063e76b87d9b
whoseJam <276707964@qq.com>
2025-08-13 10:57:44 +0800

    [work]SPFA

--- work/SPFA/SPFA概述/SPFA.js
+++ work/SPFA/SPFA概述/SPFA.js
@@ -3,48 +3,59 @@ import * as sd from "@/sd";
 const svg = sd.svg();
 const C = sd.color();
 const R = sd.rule();
-
-const graph = new sd.GridGraph(svg).width(400).height(200).cx(600).cy(300);
-const data = [
-    [1, 2, 12], [1, 3, 14], [1, 4, 16],
-    [2, 4, 7], [2, 5, 10],
-    [3, 4, 9], [3, 6, 8],
-    [4, 5, 6], [4, 6, -2],
-    [5, 6, 5], [5, 7, 3],
-    [6, 7, 4]
+const links = [
+    [1, 2, 12, "mx", "my"],
+    [1, 3, 14, "mx", "y"],
+    [1, 4, 16, "cx", "my"],
+    [2, 4, -7, "x", "my"],
+    [2, 5, 10, "cx", "my"],
+    [3, 4, 9, "x", "y"],
+    [3, 6, 8, "cx", "y"],
+    [4, 5, 6, "mx", "my"],
+    [4, 6, -2, "mx", "y"],
+    [5, 6, 5, "x", "cy"],
+    [5, 7, 3, "x", "my"],
+    [6, 7, 4, "x", "y"],
 ];
+const nodes = [
+    [1, 0.5, 0, 0, "lc"],
+    [2, 0, 0.25, "inf", "tc"],
+    [3, 1, 0.25, "inf", "bc"],
+    [4, 0.5, 0.5, "inf", "rc"],
+    [5, 0, 0.75, "inf", "tc"],
+    [6, 1, 0.75, "inf", "bc"],
+    [7, 0.5, 1, "inf", "rc"],
+];
+const graph = new sd.GridGraph(svg).width(400).height(200).cx(600).cy(300);
 
 sd.init(() => {
-    function put(nodeId, locator) {
-        const varList = new sd.VarList(svg);
-        graph.element(nodeId).childAs("varList", varList, R.aside(locator));
-        return varList;
-    }
-    graph.at(0.5, 0).newNode(1); put(1, "lc").put("dis", 0).put("inq", 1);
-    graph.at(0, 0.25).newNode(2); put(2, "tc").put("dis", Infinity).put("inq", 0);
-    graph.at(1, 0.25).newNode(3); put(3, "bc").put("dis", Infinity).put("inq", 0);
-    graph.at(0.5, 0.5).newNode(4); put(4, "rc").put("dis", Infinity).put("inq", 0);
-    graph.at(0, 0.75).newNode(5); put(5, "tc").put("dis", Infinity).put("inq", 0);
-    graph.at(1, 0.75).newNode(6); put(6, "bc").put("dis", Infinity).put("inq", 0);
-    graph.at(0.5, 1).newNode(7); put(7, "rc").put("dis", Infinity).put("inq", 0);
-    data.forEach(link => {
-        graph.newLink(link[0], link[1], link[2]);
+    nodes.forEach(node => {
+        graph.at(node[1], node[2]).newNode(node[0]);
+        graph.element(node[0]).childAs("dis", new sd.Text(svg, node[3]), R.aside(node[4]));
     });
-})
+    links.forEach(link => {
+        const x = link[3] || "cx";
+        const y = link[4] || "cy";
+        graph.newLink(link[0], link[1]);
+        graph.element(link[0], link[1]).value(link[2], R.pointAtPathByRate(0.5, x, y)).arrow();
+    });
+});
 
 sd.main(async () => {
-    await SPFA(graph);
-})
-
-async function SPFA(graph) {
-    const Q = new sd.Array(svg).x(graph.x()).y(graph.my() + 100).push(1);
-    const getDis = (x) => graph.element(x).child("varList").get("dis");
-    const putDis = (x, dis) => graph.element(x).child("varList").put("dis", dis);
-    const getInq = (x) => graph.element(x).child("varList").get("inq");
-    const putInq = (x, inq) => graph.element(x).child("varList").put("inq", inq);
-    const n = graph.nodes().length;
+    const inq = sd.make1d(100, false);
+    const Q = new sd.Array(svg)
+        .x(graph.x())
+        .y(graph.my() + 100)
+        .push(1);
+    const getDis = x => {
+        const text = graph.element(x).child("dis").text();
+        if (text === "inf") return Infinity;
+        return +text;
+    };
+    const putDis = (x, dis) => graph.element(x).child("dis").text(dis);
+    const getInq = x => inq[x];
+    const putInq = (x, inq_) => (inq[x] = inq_);
     sd.Label(Q, "队列Q");
-    graph.update();
     let cnt = 0;
     while (Q.length() > 0 && ++cnt <= 20) {
         await sd.pause();
@@ -79,4 +90,4 @@ async function SPFA(graph) {
         putInq(u, 0);
         graph.endAnimate();
     }
-}
+});
