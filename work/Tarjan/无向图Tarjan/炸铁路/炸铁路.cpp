0bd8ed1a203addf6afe46073d53a40f32e5c365e
whoseJam <276707964@qq.com>
2025-08-13 14:48:41 +0800

    [work]Tarjan

--- work/Tarjan/无向图Tarjan/炸铁路/炸铁路.cpp
+++ work/Tarjan/无向图Tarjan/炸铁路/炸铁路.cpp
@@ -1,64 +1,70 @@
-#include<bits/stdc++.h>
+#include<algorithm>
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
 using namespace std;
 
-int read(){
-	int s=0,f=1;char t=getchar();
-	while('0'>t||t>'9'){
-		if(t=='-')f=-1;
-		t=getchar();
+namespace FastIO{
+	const int L=(1<<20);
+	char buf[L],*S,*T;
+	#ifdef ONLINE_JUDGE
+	inline char getchar(){
+		if(S==T){T=(S=buf)+fread(buf,1,L,stdin);if(S==T)return EOF;}
+		return *S++;
 	}
-	while('0'<=t&&t<='9'){
-		s=(s<<1)+(s<<3)+t-'0';
-		t=getchar();
+	#endif
+	inline int read(){
+		int s=0,f=1;char t=getchar();
+		while('0'>t||t>'9'){if(t=='-')f=-1;t=getchar();}
+		while('0'<=t&&t<='9'){s=(s<<1)+(s<<3)+t-'0';t=getchar();}
+		return s*f;
 	}
-	return s*f;
 }
+using FastIO::read;
 
-const int N=155;
-const int M=5005;
-const int inf=0x3f3f3f3f;
-int prt[N],low[N],dfn[N],tot;
-int n,m;
+const int N=1000005;
+int low[N],dfn[N],prt[N],n,m;
 
-struct line{
-	int Nxt,to;
-}l[M*2];
-int h[N],cnt;
-
+struct Node{
+	int to;
+	int id;
+}; 
+vector<Node> G[N];
+vector<pair<int,int>> ans;
+int cnt,tot;
 void Link(int u,int v){
-	l[++cnt]=(line){h[u],v};h[u]=cnt;
-	l[++cnt]=(line){h[v],u};h[v]=cnt;
+	++cnt;
+	G[u].push_back({v,cnt});
+	G[v].push_back({u,cnt});
 }
-
-typedef pair<int,int> pa;
-vector<pa> ans;
-
 void Tarjan(int u){
-	low[u]=dfn[u]=++tot;
-	for(int i=h[u],v;i;i=l[i].Nxt){
-		v=l[i].to;
-		if(prt[u]==v)continue;
-		if(dfn[v]==0){
-			prt[v]=u;
+	dfn[u]=low[u]=++tot;
+	for(auto& link:G[u]){
+		int v=link.to;
+		int id=link.id;
+		if(prt[u]==id)continue;
+		if(!dfn[v]){
+			prt[v]=id;
 			Tarjan(v);
 			low[u]=min(low[u],low[v]);
-			if(low[v]>=dfn[v]){
-				ans.push_back(make_pair(min(u,v),max(u,v)));
+			if(low[v]==dfn[v]){
+				if(u<v)ans.push_back(make_pair(u,v));
+				else ans.push_back(make_pair(v,u));
 			}
-		}else low[u]=min(low[u],dfn[v]);
+		}else{
+			low[u]=min(low[u],dfn[v]);
+		}
 	}
-} 
-
+}
 int main(){
 	n=read();m=read();
-	for(int i=1,a,b;i<=m;i++){
-		a=read();b=read();
-		Link(a,b);
-	}
+	for(int i=1;i<=m;i++)Link(read(),read());
 	Tarjan(1);
 	sort(ans.begin(),ans.end());
-	for(int i=0;i<ans.size();i++)
-		cout<<ans[i].first<<' '<<ans[i].second<<'\n';
+	for(auto& link:ans){
+		cout<<link.first<<' '<<link.second<<'\n';
+	}
 	return 0;
 }
-
