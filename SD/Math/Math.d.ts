89487ae35a7c9f3f4068dc17e9cb30aecdc6f6d8
whoseJam <276707964@qq.com>
2025-10-24 22:23:04 +0800

    [feat]add Math typescript

--- SD/Math/Math.d.ts
+++
@@ -1,3 +0,0 @@
-export function equal(x: number, y: number): boolean;
-export function dqual(x: number, y: number): boolean;
-export function mapTo(left: number, length: number, newLeft: number, newLength: number): (k: number) => number;
--- SD/Math/Math.ts
+++ SD/Math/Math.ts
@@ -1,25 +1,25 @@
-function ddcmp(x) {
+function ddcmp(x: number): number {
     if (Math.abs(x) > 1e-2) return 1;
     return Math.abs(x) < -1e-2 ? -1 : 0;
 }
 
-function dcmp(x) {
+function dcmp(x: number): number {
     if (Math.abs(x) > 1) return 1;
     return Math.abs(x) < -1 ? -1 : 0;
 }
 
-export function equal(x, y) {
+export function equal(x: number, y: number): boolean {
     if (typeof x !== "number" || typeof y !== "number") return false;
     return dcmp(x - y) === 0;
 }
 
-export function dqual(x, y) {
+export function dqual(x: number, y: number): boolean {
     if (typeof x !== "number" || typeof y !== "number") return false;
     return ddcmp(x - y) === 0;
 }
 
-export function mapTo(left, length, newLeft, newLength) {
-    return function (k) {
+export function mapTo(left: number, length: number, newLeft: number, newLength: number): (k: number) => number {
+    return function (k: number): number {
         if (length === 0) return newLeft;
         return newLeft + ((k - left) / length) * newLength;
     };
