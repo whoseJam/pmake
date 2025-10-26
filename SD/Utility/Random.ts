7e406c224bf5f90c03188f06d8aedd5b9939fea5
whoseJam <276707964@qq.com>
2025-10-26 17:54:01 +0800

    [feat]Random -> TypeScript

--- SD/Utility/Random.ts
+++ SD/Utility/Random.ts
@@ -4,25 +4,25 @@ const upperCaseAlphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const specialCharacters = "!@#$%^&*()_+-=<>,.?:;'[]{}|";
 const allCharacters = numberCharacters + lowerCaseAlphabetCharacters + upperCaseAlphabetCharacters + specialCharacters;
 
-export function rand(l, r) {
-    l = +l;
-    r = +r;
-    const ans = Math.floor(Math.random() * (r - l + 2));
-    return Math.min(Math.max(l + ans, l), r);
+export function rand(l: number, r: number): number {
+    const l_ = +l;
+    const r_ = +r;
+    const ans = Math.floor(Math.random() * (r_ - l_ + 1));
+    return Math.min(Math.max(l_ + ans, l_), r_);
 }
 
-export function randChar() {
+export function randChar(): string {
     return allCharacters[rand(0, allCharacters.length - 1)];
 }
 
-export function randLowerCaseAlphabet() {
+export function randLowerCaseAlphabet(): string {
     return lowerCaseAlphabetCharacters[rand(0, lowerCaseAlphabetCharacters.length - 1)];
 }
 
-export function randUpperCaseAlphabet() {
+export function randUpperCaseAlphabet(): string {
     return upperCaseAlphabetCharacters[rand(0, upperCaseAlphabetCharacters.length - 1)];
 }
 
-export function randSpecialChar() {
+export function randSpecialChar(): string {
     return specialCharacters[rand(0, specialCharacters.length - 1)];
 }
